9e91ad089b46a0cc35858db866f58e37f106381d
thinhlv <thinhlv02@wru.vn>
2019-06-16 10:44:18 +0700

    udpate at home

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -44,7 +44,7 @@
         <!-- footer content -->
         <footer>
           <div class="pull-right">
-            Thiết kế bởi SGC
+            Thiết kế bởi phucthinhmedia
           </div>
           <div class="clearfix"></div>
         </footer>
