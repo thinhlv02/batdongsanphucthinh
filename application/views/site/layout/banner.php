bec0ec22e72c59b589d2803dff02c962e31421f2
thinhlv <thinhlv02@wru.vn>
2019-06-16 09:46:55 +0700

    udpate at home

--- application/views/site/layout/banner.php
+++ application/views/site/layout/banner.php
@@ -2,7 +2,7 @@
 
 <section class="banner" style="background-image: url(<?php echo public_url('images/'.$content->banner)?>)">
     <div class="banner-left">
-        <p class="caption-banner animated slideInLeft">Đơn1212 giản hoá công việc gia đình1212<br>vì chúng tôi luôn bên bạn</p>
+        <p class="caption-banner animated slideInLeft">Đơn giản hoá việc bán hàng<br>vì chúng tôi luôn bên bạn</p>
         <a href="<?php echo base_url('gioi-thieu-dich-vu.html')?>"
            class="btn btn-primary animated bounceIn delay-1s" style="margin: 20px 0px; padding: 12px" >TÌM HIỂU THÊM</a>
     </div>
