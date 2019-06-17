7b617850452de6202464928f07b2e1ae53de13a2
thinhlv <thinhlv02@wru.vn>
2019-06-17 22:12:17 +0700

    udpate at home

--- application/views/site/layout/layout.php
+++ application/views/site/layout/layout.php
@@ -22,4 +22,11 @@
     })();
 </script>
 
+<!--//thinhlv add-->
+<script>
+    $(".project-hot").bxSlider({auto:true,pager:true,controls:false});$("#raovat-nb").load("/Home/LoadAdsNbHome");$(".ulthumb li:gt(0)").hide();var j=1;var len=5;var first=true;$(".box-news-home").jCarouselLite({vertical:true,hoverPause:false,visible:len,start:0,warp:"circle",auto:500,speed:1000,beforeStart:function(a){if(!first)$(a).parent().delay(1000);},afterEnd:function(a){if(first){first=false;}else{$("ul.ulthumb #li_"+j).css("display","none");j++;if(j>len)j=1;$("ul.ulthumb #li_"+j).css("display","block");}
+            $(a).parent().delay(6500);}});
+</script>
+<!--//thinhlv add-->
+
 </html>
