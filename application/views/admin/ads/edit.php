d1a6ebcc2ed31ace6acce2eea304d3089035a63d
thinhlv <thinhluuvan@admicro.vn>
2019-06-18 17:34:43 +0700

    update

--- application/views/admin/ads/edit.php
+++ application/views/admin/ads/edit.php
@@ -59,6 +59,22 @@
                     </div>
                 </div>
 
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Giá
+                        <span class="required">*</span></label>
+                    <div class="col-md-8 col-sm-8 col-xs-12">
+                        <input type="text" id="" name="price" value="<?php echo $ads->price ?>" required="required" class="form-control col-md-7 col-xs-12" placeholder="Document Title">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Diện tích
+                        <span class="required">*</span></label>
+                    <div class="col-md-8 col-sm-8 col-xs-12">
+                        <input type="text" id="txtDocumentTitle" name="acreage" value="<?php echo $ads->acreage ?>" required="required" class="form-control col-md-7 col-xs-12" placeholder="Document Title">
+                    </div>
+                </div>
+
 <!--                <div class="form-group">-->
 <!--                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Meta Description-->
 <!--                        <span class="required">*</span></label>-->
