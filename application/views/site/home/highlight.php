73af7e91b220a9f3b2ae1460eac6837fbdf87e2d
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 11:24:47 +0700

    update

--- application/views/site/home/highlight.php
+++ application/views/site/home/highlight.php
@@ -4,7 +4,7 @@
             <div class="title-section"><h2>Dịch vụ đa dạng, mọi lúc mọi nơi</h2></div>
             <div class="intro-item">
                 <div class="intro-img">
-                    <img src="<?php echo public_url('images/intro/2.png')?>">
+                    <img src="<?php echo public_url('images/intro/2.png') ?>">
                 </div>
                 <div class="intro-text">
                     <h2>Dịch vụ :</h2>
@@ -28,46 +28,18 @@
                     </ol>
 
                     <div class="carousel-inner">
+                        <?php foreach ($ads as $key=>$value){
+                            echo $key. '<br/>';
+                        } ?>
                         <div class="item active">
-                            <img class="image" src="<?php echo public_url('images/intro/step1.png')?>" alt="Bước 1">
+                            <img class="image" src="<?php echo public_url('images/intro/step1.png') ?>" alt="Bước 1">
                             <div class="text">
                                 <span>Bước 1</span>
                                 <h2>Chọn dịch vụ cần làm</h2>
                                 <p>Bạn có thể chọn một trong các dịch vụ: Giúp việc theo giờ, sửa chữa điện nước, sửa chữa điện lạnh</p>
                             </div>
                         </div>
-                        <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step2.png')?>" alt="Bước 2">
-                            <div class="text">
-                                <span>Bước 2</span>
-                                <h2>Chọn địa chỉ, dịch vụ phù hợp</h2>
-                                <p>Bạn có thể nhập địa chỉ bằng tay hoặc chọn trên bản đồ, sau đó chọn dịch vụ theo nhu cầu của mình và ấn nút Tìm kỹ thuật viên/Tìm cộng tác viên. Chúng tôi sẽ tìm KTV, CTV phù hợp gần bạn nhất</p>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step3.png')?>" alt="Bước 3">
-                            <div class="text">
-                                <span>Bước 3</span>
-                                <h2>Thông báo tìm được KTV/CTV</h2>
-                                <p>Sau khi tìm được KTV/CTV phù hợp, chúng tôi sẽ thông báo đến cho bạn. KTV/CTV sẽ ngay lập tức di chuyển đến vị trí của bạn.</p>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step4.png')?>" alt="Bước 4">
-                            <div class="text">
-                                <span>Bước 4</span>
-                                <h2>Báo giá</h2>
-                                <p>KTV/CTV của chúng tôi sẽ tiến hành khảo sát và gửi báo giá đến cho bạn. Đồng ý với báo giá để KTV/CTV bắt đầu công việc.</p>
-                            </div>
-                        </div>
-                        <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step5.png')?>" alt="Bước 5">
-                            <div class="text">
-                                <span>Bước 5</span>
-                                <h2>Hoàn thành, đánh giá</h2>
-                                <p>KTV/CTV của chúng tôi hoàn thành công việc, bạn vui lòng thanh toán cho KTV/CTV và đánh giá KTV/CTV của chúng tôi</p>
-                            </div>
-                        </div>
+
                         <a class="right carousel-control" href="#myCarousel" data-slide="next">
                             <span class="glyphicon glyphicon-chevron-right"></span>
                             <span class="sr-only">Next</span>
@@ -76,20 +48,20 @@
                 </div>
             </div>
 
-<!--            <div class="intro-item">-->
-<!--                <div class="intro-img">-->
-<!--                    <img src="--><?php //echo public_url('images/intro/2.png')?><!--">-->
-<!--                </div>-->
-<!--                <div class="intro-text">-->
-<!--                    <h2>Thao tác đơn giản:</h2>-->
-<!--                    <p>-->
-<!--                        Mọi thao tác nằm gọn trong chiếc điện thoại thông minh, người dùng chỉ cần vài bước đơn giản trên màn hình điện thoại đã có thể tìm được người cung cấp dịch vụ một cách nhanh chóng, thuận tiện.-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div class="intro-item">-->
+            <!--                <div class="intro-img">-->
+            <!--                    <img src="--><?php //echo public_url('images/intro/2.png')?><!--">-->
+            <!--                </div>-->
+            <!--                <div class="intro-text">-->
+            <!--                    <h2>Thao tác đơn giản:</h2>-->
+            <!--                    <p>-->
+            <!--                        Mọi thao tác nằm gọn trong chiếc điện thoại thông minh, người dùng chỉ cần vài bước đơn giản trên màn hình điện thoại đã có thể tìm được người cung cấp dịch vụ một cách nhanh chóng, thuận tiện.-->
+            <!--                </div>-->
+            <!--            </div>-->
 
             <div class="intro-item">
                 <div class="intro-img">
-                    <img src="<?php echo public_url('images/intro/3.png')?>">
+                    <img src="<?php echo public_url('images/intro/3.png') ?>">
                 </div>
                 <div class="intro-text">
                     <h2>Dịch vụ hoàn hảo:</h2>
