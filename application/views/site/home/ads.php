dea77bae2ba22dbd48b85c9bd9cd2138c3948e7b
thinhlv <thinhlv02@wru.vn>
2019-06-19 21:29:10 +0700

    udpate at home

--- application/views/site/home/ads.php
+++ application/views/site/home/ads.php
@@ -2,11 +2,11 @@
     <div class="container">
         <div class="col-sm-12 col-md-12">
 <!--            <div class="title-section"><h2>Tin rao nổi bật</h2></div>-->
-<!--            //test-->
+<!--            test-->
             <div class="box-module mt10">
                 <div class="bg-modul"><i class="glyphicon glyphicon-th"></i> Tin rao nổi bật</div>
             </div>
-<!--            //test-->
+<!--            test-->
 <!--            <div class="sub-title-section">Khám phá tại đây</div>-->
             <div class="row">
                 <?php foreach ($ads as $key => $value) { ?>
