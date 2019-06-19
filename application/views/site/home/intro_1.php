d5563c416b7e7f3b2fb6221a42a6ae886a3031bd
thinhlv <thinhlv02@wru.vn>
2019-06-19 21:44:47 +0700

    udpate at home

--- application/views/site/home/intro_1.php
+++ application/views/site/home/intro_1.php
@@ -1,7 +1,10 @@
 <section class="intro_1">
     <div class="container">
         <div class="col-sm-12 col-md-12">
-            <div class="title-section"><h2>Dịch vụ đa dạng, mọi lúc mọi nơi</h2></div>
+<!--            <div class="title-section"><h2>Dịch vụ đa dạng, mọi lúc mọi nơi</h2></div>-->
+            <div class="box-module">
+                <div class="bg-modul"><i class="glyphicon glyphicon-th"></i>Dịch vụ đa dạng, mọi lúc mọi nơi</div>
+            </div>
             <div class="intro-item">
                 <div class="intro-img">
                     <img src="<?php echo public_url('images/intro/2.png')?>">
