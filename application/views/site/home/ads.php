b68b87b1e4b28a384eb1a614d1a371b5727b4d4d
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 14:08:05 +0700

    update

--- application/views/site/home/ads.php
+++ application/views/site/home/ads.php
@@ -1,7 +1,7 @@
 <section class="news">
     <div class="container">
         <div class="col-sm-12 col-md-12">
-            <div class="title-section"><h2>Tin rao nổi bật</h2></div>
+            <div class="title-section"><h2>Tin rao nổi bật12</h2></div>
             <div class="sub-title-section">Khám phá tại đây</div>
             <div class="row">
                 <?php foreach ($ads as $key => $value) { ?>
