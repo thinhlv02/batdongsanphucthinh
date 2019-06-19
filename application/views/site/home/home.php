9c8dac143f55b976e8049a7a01f8bb19effd3938
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 09:15:12 +0700

    update

--- application/views/site/home/home.php
+++ application/views/site/home/home.php
@@ -1,9 +1,11 @@
+<!--//thinhlv add-->
 <?php //$this->load->view('site/home/clearfix') ?>
+<!--end thinhlv add-->
 <?php $this->load->view('site/home/intro_1') ?>
 <?php $this->load->view('site/home/service') ?>
 <?php $this->load->view('site/home/intro_2') ?>
 <?php //$this->load->view('site/home/download') ?>
 <?php $this->load->view('site/home/news')?>
-<?php //$this->load->view('site/home/product')?>
-<?php //$this->load->view('site/partner/partner')?>
-<?php //$this->load->view('site/recruitment')?>
+<?php $this->load->view('site/home/product')?>
+<?php $this->load->view('site/partner/partner')?>
+<?php $this->load->view('site/recruitment')?>
