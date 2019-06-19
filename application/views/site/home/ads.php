203ccd2af7d8b086903f0757f19168ed8af83ebb
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 13:47:09 +0700

    update

--- application/views/site/home/ads.php
+++ application/views/site/home/ads.php
@@ -7,17 +7,17 @@
             <?php foreach ($ads as $key=>$value){ ?>
                 <?php if($key > 0){ ?>
                     <div class="col-md-4 col-sm-6 col-xs-12">
-                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->name).'-'.$value->id)?>">
+                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->title).'-'.$value->id)?>">
                             <div class="item-news">
                                 <div class="img-news" style="background-image: url(<?php echo public_url('images/news/'.$value->img)?>)">
                                     <div class="content-news">
-                                        <div><strong style="color: #fff"><?php echo $value->name?></strong></div>
+                                        <div><strong style="color: #fff"><?php echo $value->title?></strong></div>
                                         <p style="color: #fff"><?php echo $value->intro?></p>
-                                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->name).'-'.$value->id)?>" class="btn btn-primary" style="margin-top: 10px">Xem thêm</a>
+                                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->title).'-'.$value->id)?>" class="btn btn-primary" style="margin-top: 10px">Xem thêm</a>
                                     </div>
                                     <div class="title-news">
                                         <span style="font-size: 13px"><?php echo date('d/m/Y', strtotime($value->created_at))?></span><br>
-                                        <strong><?php echo $value->name?></strong>
+                                        <strong><?php echo $value->title?></strong>
                                     </div>
 
                                 </div>
@@ -26,17 +26,17 @@
                     </div>
                 <?php } else{ ?>
                     <div class="col-md-8 col-sm-12 col-xs-12">
-                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->name).'-'.$value->id)?>">
+                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->title).'-'.$value->id)?>">
                             <div class="item-news">
                                 <div class="img-news" style="background-image: url(<?php echo public_url('images/news/'.$value->img)?>)">
                                     <div class="content-news">
-                                        <div><strong><?php echo $value->name?></strong></div>
+                                        <div><strong><?php echo $value->title?></strong></div>
                                         <p><?php echo $value->intro?></p>
-                                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->name).'-'.$value->id)?>" class="btn btn-primary" style="margin-top: 10px">Xem thêm</a>
+                                        <a href="<?php echo base_url('tin-tuc/'.create_slug($value->title).'-'.$value->id)?>" class="btn btn-primary" style="margin-top: 10px">Xem thêm</a>
                                     </div>
                                     <div class="title-news">
                                         <span style="font-size: 13px"><?php echo date('d/m/Y', strtotime($value->created_at))?></span><br>
-                                        <strong><?php echo $value->name?></strong>
+                                        <strong><?php echo $value->title?></strong>
                                     </div>
 
                                 </div>
