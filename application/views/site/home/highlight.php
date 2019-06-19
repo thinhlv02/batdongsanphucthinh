8f0b981597223414ed06281f5471543ed32912ba
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 11:29:14 +0700

    update

--- application/views/site/home/highlight.php
+++ application/views/site/home/highlight.php
@@ -22,9 +22,9 @@
                     <ol class="carousel-indicators">
                         <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
                         <li data-target="#myCarousel" data-slide-to="1"></li>
-                        <li data-target="#myCarousel" data-slide-to="2"></li>
-                        <li data-target="#myCarousel" data-slide-to="3"></li>
-                        <li data-target="#myCarousel" data-slide-to="4"></li>
+<!--                        <li data-target="#myCarousel" data-slide-to="2"></li>-->
+<!--                        <li data-target="#myCarousel" data-slide-to="3"></li>-->
+<!--                        <li data-target="#myCarousel" data-slide-to="4"></li>-->
                     </ol>
 
                     <div class="carousel-inner">
