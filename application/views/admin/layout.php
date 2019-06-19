e1068fc2d85eeef370e43794c5c3890e76b7e18e
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 09:29:48 +0700

    update

--- application/views/admin/layout.php
+++ application/views/admin/layout.php
@@ -44,7 +44,7 @@
         <!-- footer content -->
         <footer>
           <div class="pull-right">
-            Thiết kế bởi phucthinhmedia
+            Thiết kế bởi SGC
           </div>
           <div class="clearfix"></div>
         </footer>
