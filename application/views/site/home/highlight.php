5db141d75136471a24f8d615a272cd064e14a83f
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 10:21:27 +0700

    update

--- application/views/site/home/highlight.php
+++ application/views/site/home/highlight.php
@@ -4,14 +4,16 @@
             <div class="title-section"><h2>Dịch vụ đa dạng, mọi lúc mọi nơi</h2></div>
             <div class="intro-item">
                 <div class="intro-img">
-                    <img src="<?php echo public_url('images/intro/2.png')?>">
+                    <img src="<?php echo public_url('images/intro/2.png') ?>">
                 </div>
                 <div class="intro-text">
-                    <h2>Dịch vụ GaiA:</h2>
+                    <h2>Dịch vụ:</h2>
                     <p>
-                        - Giúp việc theo giờ <br>
-                        - Vệ sinh văn phòng <br>
-                        - Sửa chữa, bảo dưỡng, tư vấn, lắp đặt các thiết bị điện lạnh, điện nước
+                        - Đăng tin rao vặt 1 <br>
+                        - Đăng tin rao vặt 2 <br>
+                        - Đăng tin rao vặt 3 <br>
+                        - Đăng tin rao vặt 4 <br>
+                        - Đăng tin rao vặt
                     </p>
                 </div>
             </div>
@@ -28,7 +30,7 @@
 
                     <div class="carousel-inner">
                         <div class="item active">
-                            <img class="image" src="<?php echo public_url('images/intro/step1.png')?>" alt="Bước 1">
+                            <img class="image" src="<?php echo public_url('images/intro/step1.png') ?>" alt="Bước 1">
                             <div class="text">
                                 <span>Bước 1</span>
                                 <h2>Chọn dịch vụ cần làm</h2>
@@ -36,7 +38,7 @@
                             </div>
                         </div>
                         <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step2.png')?>" alt="Bước 2">
+                            <img class="image" src="<?php echo public_url('images/intro/step2.png') ?>" alt="Bước 2">
                             <div class="text">
                                 <span>Bước 2</span>
                                 <h2>Chọn địa chỉ, dịch vụ phù hợp</h2>
@@ -44,7 +46,7 @@
                             </div>
                         </div>
                         <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step3.png')?>" alt="Bước 3">
+                            <img class="image" src="<?php echo public_url('images/intro/step3.png') ?>" alt="Bước 3">
                             <div class="text">
                                 <span>Bước 3</span>
                                 <h2>Thông báo tìm được KTV/CTV</h2>
@@ -52,7 +54,7 @@
                             </div>
                         </div>
                         <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step4.png')?>" alt="Bước 4">
+                            <img class="image" src="<?php echo public_url('images/intro/step4.png') ?>" alt="Bước 4">
                             <div class="text">
                                 <span>Bước 4</span>
                                 <h2>Báo giá</h2>
@@ -60,7 +62,7 @@
                             </div>
                         </div>
                         <div class="item">
-                            <img class="image" src="<?php echo public_url('images/intro/step5.png')?>" alt="Bước 5">
+                            <img class="image" src="<?php echo public_url('images/intro/step5.png') ?>" alt="Bước 5">
                             <div class="text">
                                 <span>Bước 5</span>
                                 <h2>Hoàn thành, đánh giá</h2>
@@ -75,20 +77,20 @@
                 </div>
             </div>
 
-<!--            <div class="intro-item">-->
-<!--                <div class="intro-img">-->
-<!--                    <img src="--><?php //echo public_url('images/intro/2.png')?><!--">-->
-<!--                </div>-->
-<!--                <div class="intro-text">-->
-<!--                    <h2>Thao tác đơn giản:</h2>-->
-<!--                    <p>-->
-<!--                        Mọi thao tác nằm gọn trong chiếc điện thoại thông minh, người dùng chỉ cần vài bước đơn giản trên màn hình điện thoại đã có thể tìm được người cung cấp dịch vụ một cách nhanh chóng, thuận tiện.-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div class="intro-item">-->
+            <!--                <div class="intro-img">-->
+            <!--                    <img src="--><?php //echo public_url('images/intro/2.png')?><!--">-->
+            <!--                </div>-->
+            <!--                <div class="intro-text">-->
+            <!--                    <h2>Thao tác đơn giản:</h2>-->
+            <!--                    <p>-->
+            <!--                        Mọi thao tác nằm gọn trong chiếc điện thoại thông minh, người dùng chỉ cần vài bước đơn giản trên màn hình điện thoại đã có thể tìm được người cung cấp dịch vụ một cách nhanh chóng, thuận tiện.-->
+            <!--                </div>-->
+            <!--            </div>-->
 
             <div class="intro-item">
                 <div class="intro-img">
-                    <img src="<?php echo public_url('images/intro/3.png')?>">
+                    <img src="<?php echo public_url('images/intro/3.png') ?>">
                 </div>
                 <div class="intro-text">
                     <h2>Dịch vụ hoàn hảo:</h2>
