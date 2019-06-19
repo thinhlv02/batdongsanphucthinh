72a356b9fb6b1d06604bfb67ec1179f8b1816803
thinhlv <thinhluuvan@admicro.vn>
2019-06-19 17:32:03 +0700

    update

--- application/views/admin/left.php
+++ application/views/admin/left.php
@@ -1,8 +1,15 @@
 <div class="col-md-3 left_col">
     <div class="left_col scroll-view">
+<!--        <div class="navbar nav_title" style="border: 0;">-->
+<!--            <a href="#" class="site_title"><i class="fa fa-paw"></i> <span>Trang quản lý</span></a>-->
+<!--        </div>-->
+
         <div class="navbar nav_title" style="border: 0;">
-            <a href="#" class="site_title"><i class="fa fa-paw"></i> <span>Trang quản lý</span></a>
+            <a href="<?php echo admin_url('dashboard') ?>" class="site_title"><i class="fa fa-paw"></i>
+                <span>Trang chủ</span></a>
         </div>
+
+        <div class="clearfix"></div>
 
         <div class="clearfix"></div>
 
