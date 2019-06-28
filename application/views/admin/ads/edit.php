05b8bbce64e5f5139cef4fa967add6c8ecca0462
thinhlv <thinhlv02@wru.vn>
2019-06-28 19:15:19 +0700

    udpate at home

--- application/views/admin/ads/edit.php
+++ application/views/admin/ads/edit.php
@@ -59,7 +59,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Diện tích<span class="required">*</span></label>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Diện tích12<span class="required">*</span></label>
                     <div class="col-md-2 col-sm-2 col-xs-12">
                         <input type="number" id="" name="acreage" value="<?php echo $ads->acreage ?>" required="required" class="form-control col-md-7 col-xs-12" placeholder="ví dụ: 90">
                     </div>
