1e8079574bcc57ff8c2da862dfd260b448e1c2c3
thinhlv <thinhluuvan@admicro.vn>
2019-06-29 09:09:31 +0700

    update

--- application/views/site/pages/ads_detail.php
+++ application/views/site/pages/ads_detail.php
@@ -10,7 +10,7 @@
                 <span><?php echo date('d/m/Y', strtotime($ads->created_at)) ?></span>
                 <h2 class="title-news-1"><?php echo $ads->title ?></h2>
                 <div class="img-news-1">
-                    <img src="<?php echo public_url('ges/ads/' . $ads->img) ?>">
+                    <img src="<?php echo public_url('images/ads/' . $ads->img) ?>">
                 </div>
                 <div class="content-news-1" style="margin-top: 20px">
                     <?php echo $ads->content ?>
@@ -24,7 +24,7 @@
                 <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
                     <div class="item-news-1">
                         <div class="img-news-1">
-                            <img src="<?php echo public_url('ges/ads/' . $value->img) ?>">
+                            <img src="<?php echo public_url('images/ads/' . $value->img) ?>">
                         </div>
                         <div style="padding: 10px">
                             <h2 class="title-news-1"><?php echo $value->title ?></h2>
