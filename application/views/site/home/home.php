d24ea02b944d9599a48e418b5f46f2eeeee8d724
thinhlv <thinhlv02@wru.vn>
2019-07-03 21:27:12 +0700

    udpate at home

--- application/views/site/home/home.php
+++ application/views/site/home/home.php
@@ -1,4 +1,5 @@
 <?php //$this->load->view('site/home/intro_1') ?>
+<?php $this->load->view('site/home/test')?>
 <?php $this->load->view('site/home/ads')?>
 <?php $this->load->view('site/home/highlight') ?>
 <?php $this->load->view('site/home/service') ?>
---
+++ application/views/site/home/test.php
@@ -0,0 +1,15 @@
+<section class="news">
+    <div class="container">
+        <div class="row">
+
+
+            <div class="col-sm-8 col-md-8">8888888888
+            </div>
+
+            <div class="col-sm-8 col-md-8">4444444
+            </div>
+
+
+        </div>
+    </div>
+</section>
