db3f83eada8655f5c7d6cf5e8685e10d8758e8be
thinhlv <thinhluuvan@admicro.vn>
2019-07-04 14:25:22 +0700

    update

--- application/views/admin/ads/add.php
+++ application/views/admin/ads/add.php
@@ -70,12 +70,12 @@
 
                 </div>
 
-                <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tiêu đề tin trên web <span class="required">*</span></label>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <input type="text" id="txtDocumentTitle" name="txtDocumentTitle" required="required" class="form-control col-md-7 col-xs-12" placeholder="Tiêu đề tin trên web">
-                    </div>
-                </div>
+<!--                <div class="form-group">-->
+<!--                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tiêu đề tin trên web <span class="required">*</span></label>-->
+<!--                    <div class="col-md-8 col-sm-8 col-xs-12">-->
+<!--                        <input type="text" id="txtDocumentTitle" name="txtDocumentTitle" required="required" class="form-control col-md-7 col-xs-12" placeholder="Tiêu đề tin trên web">-->
+<!--                    </div>-->
+<!--                </div>-->
 
                 <div class="form-group">
                     <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Khu vực<span class="required">*</span></label>
