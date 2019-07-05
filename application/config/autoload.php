7a1e39adf08fe770ae9a82dab90eaf64b1680b68
thinhlv <thinhluuvan@admicro.vn>
2019-07-05 11:29:03 +0700

    update

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -103,7 +103,7 @@ $autoload['helper'] = array('url','file','common');
 | config files.  Otherwise, leave it blank.
 |
 */
-$autoload['config'] = array();
+$autoload['config'] = array('config_custom');
 
 /*
 | -------------------------------------------------------------------
