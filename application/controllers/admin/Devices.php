a5b475a2b633c217b36b3cbfd4ccf969d651da6f
thinhlv <thinhluuvan@admicro.vn>
2019-07-05 11:48:03 +0700

    update

--- application/controllers/admin/Devices.php
+++ application/controllers/admin/Devices.php
@@ -96,7 +96,7 @@ Class Devices extends MY_Controller
         }
         $this->data['tab'] = 3;
         $this->data['devices'] = $devices;
-        var_dump($devices);
+//        var_dump($devices);
         $this->data['device_type'] = $this->_device_type;
         $this->data['temp'] = 'admin/devices/index';
         $this->data['view'] = 'admin/devices/edit';
