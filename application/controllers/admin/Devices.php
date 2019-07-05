1c02caa684b5b5634f7fd8ca02c73b01114f4415
thinhlv <thinhluuvan@admicro.vn>
2019-07-05 14:35:54 +0700

    update

--- application/controllers/admin/Devices.php
+++ application/controllers/admin/Devices.php
@@ -22,6 +22,7 @@ Class Devices extends MY_Controller
 
         foreach ($devices as $key => $val) {
             $devices_end = new stdClass();
+            $devices_end[$key] = 12;
 //            $devices_end[$key]->id = $val->id;
         }
         echo '<pre>',print_r($devices_end,1),'</pre>';
