a6c1aa1dfec2adac70c48390f1a74aa9690c5469
thinhlv <thinhluuvan@admicro.vn>
2019-07-05 14:08:54 +0700

    update

--- application/views/admin/devices/edit.php
+++ application/views/admin/devices/edit.php
@@ -70,7 +70,7 @@
                 <div class="form-group">
                     <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Giá tiền <span class="required">*</span></label>
                     <div class="col-md-8 col-sm-8 col-xs-12">
-                        <input type="number" id="" name="txtPrice" value="<?php echo $value->price ?>" class="form-control col-md-7 col-xs-12" placeholder="Giá tiền">
+                        <input type="number" id="" name="txtPrice" value="<?php echo $devices->price ?>" class="form-control col-md-7 col-xs-12" placeholder="Giá tiền">
                     </div>
                 </div>
 
