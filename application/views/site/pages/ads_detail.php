1c0c95a10ff2c7a4045d53d5b3d400b081185c5b
thinhlv <thinhlv02@wru.vn>
2019-07-08 23:23:32 +0700

    udpate at home

--- application/views/site/pages/ads_detail.php
+++ application/views/site/pages/ads_detail.php
@@ -131,7 +131,9 @@
 
         <div class="col-md-4 col-sm-12 col-xs-12 mt-2" style="border: 1px solid #dedede;">
             <div class="left-title">Nổi bật cùng chuyên mục</div>
-            <?php foreach ($highlight as $key => $value) { ?>
+            <?php foreach ($highlight as $key => $value) {
+                if ($ads->id != $value->id) {
+                ?>
                 <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
                     <div class="mb-5" style="border: 1px solid #dedede;float: left">
                         <div class="col-md-6 col-sm-6 col-xs-6">
@@ -143,7 +145,7 @@
                         </div>
                     </div>
                 </a>
-            <?php } ?>
+            <?php } } ?>
         </div>
     </div>
 </section>
