c2c4cf3d9ff7d934a0bb3bbdfa273b5b60c87372
thinhlv <thinhluuvan@admicro.vn>
2019-07-09 13:57:31 +0700

    update

--- application/views/site/home/ads.php
+++ application/views/site/home/ads.php
@@ -12,22 +12,24 @@
                 <?php foreach ($vip as $key => $value) { ?>
 
                     <div class="col-md-3 col-sm-6 col-xs-12 card">
-                        <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
-                            <div class="item-news">
-                                <div class="img-news" style="background-image: url(<?php echo public_url('images/ads/' . $value->img) ?>)">
-                                    <div class="content-news">
-                                        <div><strong style="color: #fff"><?php echo $value->title ?></strong></div>
-                                        <p style="color: #fff"><?php echo $value->intro ?></p>
-                                        <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>" class="btn btn-primary" style="margin-top: 10px">Xem thêm</a>
-                                    </div>
-                                    <div class="title-news">
-                                        <span style="font-size: 13px"><?php echo date('d/m/Y', strtotime($value->created_at)) ?></span><br>
-                                        <strong><?php echo $value->title ?></strong>
-                                    </div>
+                        <div class="card-body mb-2">
+                            <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
+                                <div class="item-news">
+                                    <div class="img-news" style="background-image: url(<?php echo public_url('images/ads/' . $value->img) ?>)">
+                                        <div class="content-news">
+                                            <div><strong style="color: #fff"><?php echo $value->title ?></strong></div>
+                                            <p style="color: #fff"><?php echo $value->intro ?></p>
+                                            <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>" class="btn btn-primary" style="margin-top: 10px">Xem thêm</a>
+                                        </div>
+                                        <div class="title-news">
+                                            <span style="font-size: 13px"><?php echo date('d/m/Y', strtotime($value->created_at)) ?></span><br>
+                                            <strong><?php echo $value->title ?></strong>
+                                        </div>
 
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
+                            </a>
+                        </div>
                     </div>
 
                 <?php } ?>
