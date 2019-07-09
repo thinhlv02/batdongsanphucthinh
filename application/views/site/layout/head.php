a9cf05f8baefa73a1cb1a6dd3ad7bcb436116359
Admin <thinhlv02@wru.vn>
2019-07-09 19:57:43 +0700

    dcm

--- application/views/site/layout/head.php
+++ application/views/site/layout/head.php
@@ -12,12 +12,10 @@
 <meta property="og:description" content="<?php echo isset($description) && $description ? $description : 'Bất động sản phúc thịnh - Kênh thông tin số 1 về bất động sản tại Việt Nam. Mang đến nguồn thông tin mua bán và cho thuê nhà đất, văn phòng, chung cư... Cập nhật tin tức bất động sản nhanh nhất và chính xác nhất'?>" />
 <meta property="og:url" content="<?php echo isset($page_url) ? $page_url : base_url();?>" />
 <meta property="og:site_name" content="Bất động sản phúc thịnh - Kênh thông tin số 1 về bất động sản tại Việt Nam. Mang đến nguồn thông tin mua bán và cho thuê nhà đất, văn phòng, chung cư... Cập nhật tin tức bất động sản nhanh nhất và chính xác nhất" />
-<!--<meta property="og:image" content="--><?php //echo isset($image) ? $image : public_url('images/og_image.png')?><!--" />-->
+<meta property="og:image" content="<?php echo isset($image) ? $image : public_url('images/og_image.png')?>" />
 <meta property="og:image:width" content="400" />
 <meta property="og:image:height" content="200" />
 <meta property="og:type" content="website" />
-
-<meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
