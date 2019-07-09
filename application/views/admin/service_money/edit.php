3a1897c80d2b886eb7ff308ba30e2a2cad71d574
thinhlv <thinhluuvan@admicro.vn>
2019-07-09 15:54:02 +0700

    update

--- application/views/admin/service_money/edit.php
+++ application/views/admin/service_money/edit.php
@@ -56,6 +56,7 @@
                         <span class="required">*</span></label>
                     <div class="col-md-8 col-sm-8 col-xs-12">
                        <select class="form-control" name="make_money_by">
+                           <option value="0">Không</option>
                            <?php foreach ($emps as $k => $val) {?>
 
                             <option value="<?php echo $val->id ?>" <?php echo $val->id == $ads->make_money_by ? 'selected="selected"' : ''; ?> ><?php echo $val->name ?></option>
