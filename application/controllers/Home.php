f1ed524c63b9a766ef54d9870303dd332ea17cef
thinhlv <thinhluuvan@admicro.vn>
2019-07-10 13:54:11 +0700

    update

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -299,31 +299,21 @@ Class Home extends MY_Controller
 
     function update_view() {
 
-
         $id = $this->input->get('id');
         $ads = $this->ads_model->get_info($id);
         $view_old = $ads->view;
         $view_new = $view_old + 14;
 
         if ($ads) {
-
             $dataSubmit = array();
-
             $dataSubmit['view'] = $view_new;
-            var_dump($id. '-----');
-
-            var_dump($dataSubmit);
-
-//            $this->ads_model->update($id, $dataSubmit);
-//
-//            echo $view_new;
+//            var_dump($id. '-----');
+//            var_dump($dataSubmit);
+            $this->ads_model->update($id, $dataSubmit);
+            echo $id.'=>'.$view_new;
         }
 
-
 //        echo json_encode($res);
-
-
-
 
 //        echo 'dcm';
 //        die();
