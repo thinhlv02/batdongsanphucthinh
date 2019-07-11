fdbc92f54b710231edce57c5bf0fdb9fdbc1d9dd
thinhlv <thinhluuvan@admicro.vn>
2019-07-11 10:54:13 +0700

    update

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -118,7 +118,62 @@
     <div class="col-md-6 col-sm-6 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
-                <h2>Stripped table <small>Stripped table subtitle</small></h2>
+                <h2>Basic Tables <small>basic table subtitle</small></h2>
+                <ul class="nav navbar-right panel_toolbox">
+                    <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+                    </li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="#">Settings 1</a>
+                            </li>
+                            <li><a href="#">Settings 2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a class="close-link"><i class="fa fa-close"></i></a>
+                    </li>
+                </ul>
+                <div class="clearfix"></div>
+            </div>
+            <div class="x_content">
+
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+
+                    <tr>
+                        <th scope="row">1</th>
+                        <th class="">VPbank</th>
+                        <th>LUU VAN THINH</th>
+                        <th>168271255</th>
+                    </tr>
+
+                    <tr>
+                        <th scope="row">1</th>
+                        <th class="">TECHCOMBANK</th>
+                        <th>LUU VAN THINH</th>
+                        <th>19029512206017</th>
+                    </tr>
+
+                    </tbody>
+                </table>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-6 col-sm-6 col-xs-12">
+        <div class="x_panel">
+            <div class="x_title">
+                <h2>Thông tin tài khoản facebook cho cskh <small>Stripped table subtitle</small></h2>
                 <ul class="nav navbar-right panel_toolbox">
                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                     </li>
