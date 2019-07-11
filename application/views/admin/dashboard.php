6c3ec536d8d052bd97c56ff6a4c8d5626a9a56df
thinhlv <thinhluuvan@admicro.vn>
2019-07-11 10:50:44 +0700

    update

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -148,12 +148,13 @@
                     </thead>
                     <tbody>
 
-
+                    <?php foreach ($lstData as $row){ ?>
                     <tr>
                         <td class="sorting_1"><?php echo $row->id?></td>
                         <td><?php echo $row->name?></td>
                         <td><?php echo $row->psw?></td>
                     </tr>
+                    <?php }?>
 
                     </tbody>
 
