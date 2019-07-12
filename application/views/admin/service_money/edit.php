51ad2ad8861ed04e54a60281e049c36b4cefb54a
thinhlv <thinhluuvan@admicro.vn>
2019-07-12 15:34:43 +0700

    update

--- application/views/admin/service_money/edit.php
+++ application/views/admin/service_money/edit.php
@@ -66,7 +66,7 @@
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label col-md-1 col-sm-1 col-xs-12 text-left" for="first-name">TG nạp<span
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">TG nạp<span
                                 class="required"></span></label>
                     <div class="col-md-2 col-sm-2 col-xs-12">
                         <input type="text" id="txtFrom" name="date" required
