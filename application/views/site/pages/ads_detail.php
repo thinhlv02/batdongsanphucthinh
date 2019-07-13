d0b8015947fc4a5f94b58b965317ee05e624340a
thinhlv <thinhluuvan@admicro.vn>
2019-07-13 09:49:39 +0700

    update

--- application/views/site/pages/ads_detail.php
+++ application/views/site/pages/ads_detail.php
@@ -35,19 +35,19 @@
                         </div>
                         <div class="block">
                             <div class="col-left">Địa chỉ:</div>
-                            <div class="col-right"><b><?php echo $ads->area ?></b></div>
+                            <div class="col-right"><?php echo $ads->area ?></div>
                         </div>
                         <div class="block">
                             <div class="col-left">Diện tích</div>
-                            <b class="col-right"><span><b><?php echo $ads->acreage ?> m</span><sup>2</sup></b></div>
+                            <div class="col-right"><span><?php echo $ads->acreage ?> m</span><sup>2</sup></div>
                         </div>
                         <div class="block">
                             <div class="col-left">Giá</div>
-                            <div class="col-right"><b><?php echo $ads->price ?> (VNĐ)</b></div>
+                            <div class="col-right"><?php echo $ads->price ?> (VNĐ)</div>
                         </div>
                         <div class="block">
                             <div class="col-left">Ngày đăng</div>
-                            <div class="col-right"><b><?php echo date('d-m-Y', strtotime($ads->created_at)) ?></b></div>
+                            <div class="col-right"><?php echo date('d-m-Y', strtotime($ads->created_at)) ?></div>
                         </div>
                     </div>
 
