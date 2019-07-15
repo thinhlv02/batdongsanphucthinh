b571857463daa6ecd9e584fc83ef7047b52f5109
thinhlv <thinhlv02@wru.vn>
2019-07-15 22:43:16 +0700

    udpate at home

--- application/views/site/home/banner.php
+++ application/views/site/home/banner.php
@@ -3,107 +3,108 @@
         <!--        <div class="box-module">-->
         <!--            <div class="bg-modul"><i class="glyphicon glyphicon-star"></i> Tin Vip</div>-->
         <!--        </div>-->
-        <div class="col-sm-3 col-md-3">
-            <div class="card">
-                <div class="card-body">
+        <div class="row">
+            <div class="col-sm-3 col-md-3">
+                <div class="card">
+                    <div class="card-body">
 
 
-<!--                    <div class="box-module">-->
-<!--                        <div class="bg-modul"><i class="glyphicon glyphicon-search"></i> Tìm kiếm</div>-->
-<!--                    </div>-->
-                    <div class="page-title text-center">
-                        <h4>Tìm kiếm</h4>
+    <!--                    <div class="box-module">-->
+    <!--                        <div class="bg-modul"><i class="glyphicon glyphicon-search"></i> Tìm kiếm</div>-->
+    <!--                    </div>-->
+                        <div class="page-title text-center">
+                            <h4>Tìm kiếm</h4>
+                        </div>
+
+                        <form action="">
+                            <!--                <div class="form-group">-->
+                            <!--                    <label for="email">Mã tin:</label>-->
+                            <!--                    <input type="email" class="form-control" id="email" placeholder="Nhập mã tin" name="code">-->
+                            <!--                </div>-->
+                            <div class="form-group">
+                                <label for="email">Tỉnh thành</label>
+                                <select class="form-control">
+                                    <option value=""> -- Chọn Tỉnh thành --</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Quận Huyện</label>
+                                <select class="form-control">
+                                    <option value=""> -- Chọn Quận Huyện --</option>
+                                </select>
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email">Xã / Phường</label>
+                                <select class="form-control">
+                                    <option value=""> -- Xã / Phường --</option>
+                                </select>
+                            </div>
+
+    <!--                        <div class="form-group">-->
+    <!--                            <label for="email">Loại</label>-->
+    <!--                            <select class="form-control">-->
+    <!--                                <option value=""> -- Loại --</option>-->
+    <!--                            </select>-->
+    <!--                        </div>-->
+
+                            <button type="submit" class="btn btn-primary">Tìm kiếm</button>
+
+                        </form>
+
                     </div>
-
-                    <form action="">
-                        <!--                <div class="form-group">-->
-                        <!--                    <label for="email">Mã tin:</label>-->
-                        <!--                    <input type="email" class="form-control" id="email" placeholder="Nhập mã tin" name="code">-->
-                        <!--                </div>-->
-                        <div class="form-group">
-                            <label for="email">Tỉnh thành</label>
-                            <select class="form-control">
-                                <option value=""> -- Chọn Tỉnh thành --</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="email">Quận Huyện</label>
-                            <select class="form-control">
-                                <option value=""> -- Chọn Quận Huyện --</option>
-                            </select>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="email">Xã / Phường</label>
-                            <select class="form-control">
-                                <option value=""> -- Xã / Phường --</option>
-                            </select>
-                        </div>
-
-<!--                        <div class="form-group">-->
-<!--                            <label for="email">Loại</label>-->
-<!--                            <select class="form-control">-->
-<!--                                <option value=""> -- Loại --</option>-->
-<!--                            </select>-->
-<!--                        </div>-->
-
-                        <button type="submit" class="btn btn-primary">Tìm kiếm</button>
-
-                    </form>
-
                 </div>
+
             </div>
+            <div class="col-sm-6 col-md-6">
+                <div class="card">
+                    <div class="card-body">
 
-        </div>
-        <div class="col-sm-6 col-md-6">
-            <div class="card">
-                <div class="card-body">
+                        <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                            <!-- Indicators -->
+                            <ol class="carousel-indicators">
+                                <?php foreach ($ads_left as $key => $value) {
+                                    ?>
+                                    <li data-target="#myCarousel" data-slide-to="<?php echo $key ?>" class="<?php if ($key == 0) echo 'active'; ?>"></li>
+                                <?php } ?>
+                            </ol>
 
-                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <?php foreach ($ads_left as $key => $value) {
-                                ?>
-                                <li data-target="#myCarousel" data-slide-to="<?php echo $key ?>" class="<?php if ($key == 0) echo 'active'; ?>"></li>
-                            <?php } ?>
-                        </ol>
-
-                        <!-- Wrapper for slides -->
-                        <div class="carousel-inner">
-                            <?php foreach ($ads_left as $key => $value) {
-//                        echo $key;
-                                ?>
-                                <div class="item <?php if ($key == 0) echo 'active'; ?>">
-<!--                                    <img src="--><?php //echo public_url('images/ads/' . $value->img) ?><!--" alt="$value->img" style="width:100% ">-->
-                                    <img src="<?php echo public_url('images/ads/' . $value->img) ?>" alt="$value->img" style="width:590px; height: 295px"">
-                                    <div class="carousel-caption">
-                                        <h3><a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
-                                                <?php echo $value->title ?>
-                                            </a></h3>
-                                        <p><?php echo $value->intro ?></p>
+                            <!-- Wrapper for slides -->
+                            <div class="carousel-inner">
+                                <?php foreach ($ads_left as $key => $value) {
+    //                        echo $key;
+                                    ?>
+                                    <div class="item <?php if ($key == 0) echo 'active'; ?>">
+    <!--                                    <img src="--><?php //echo public_url('images/ads/' . $value->img) ?><!--" alt="$value->img" style="width:100% ">-->
+                                        <img src="<?php echo public_url('images/ads/' . $value->img) ?>" alt="$value->img" style="width:590px; height: 295px"">
+                                        <div class="carousel-caption">
+                                            <h3><a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
+                                                    <?php echo $value->title ?>
+                                                </a></h3>
+                                            <p><?php echo $value->intro ?></p>
+                                        </div>
                                     </div>
-                                </div>
-                            <?php } ?>
+                                <?php } ?>
 
+                            </div>
+
+                            <!-- Left and right controls -->
+                            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                                <span class="glyphicon glyphicon-chevron-left"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                                <span class="glyphicon glyphicon-chevron-right"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
 
-                        <!-- Left and right controls -->
-                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                            <span class="glyphicon glyphicon-chevron-left"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                            <span class="glyphicon glyphicon-chevron-right"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
+
                     </div>
-
-
                 </div>
             </div>
-        </div>
 
-        <div class="col-sm-3 col-md-3">
+            <div class="col-sm-3 col-md-3">
             <div class="card">
                 <div class="card-body">
 
@@ -173,6 +174,7 @@
             </div>
         </div>
 
+         </div>
     </div>
 
 </section>
