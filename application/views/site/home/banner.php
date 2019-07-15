a5ff8585078e39b05f96bc77b84d4bd06fffa142
thinh <thinhlv02@wru.vn>
2019-07-15 19:58:52 +0700

    vananh

--- application/views/site/home/banner.php
+++ application/views/site/home/banner.php
@@ -107,27 +107,64 @@
                     <div class="box-module mt-2">
                         <div class="bg-modul"><i class="glyphicon glyphicon-file"></i>Tin tức mới</div>
                     </div>
-                    <div class="box-links-right">
-                        <p>
-                            <span style="font-size:12px">
-                                <span  style="font-family:tahoma,geneva,sans-serif"><strong>KHU VỰC&nbsp;HÀ NỘI</strong></span></span>
-                        </p>
 
-                        <ul>
+                    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <?php foreach ($banner_center as $key => $value) {
+                                ?>
+                                <li data-target="#myCarousel" data-slide-to="<?php echo $key ?>" class="<?php if ($key == 0) echo 'active'; ?>"></li>
+                            <?php } ?>
+                        </ol>
 
-                            <?php foreach ($news as $key=>$value){ ?>
-
-                            <li>
-                                <a href="<?php echo base_url('tin-tuc/'.create_slug($value->name).'-'.$value->id)?>">
-                                    <?php echo $value->name?>
-                                </a>
-                            </li>
-
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner">
+                            <?php foreach ($banner_center as $key => $value) {
+//                        echo $key;
+                                ?>
+                                <div class="item <?php if ($key == 0) echo 'active'; ?>">
+                                    <img src="<?php echo public_url('images/ads/' . $value->img) ?>" alt="$value->img" style="width:100% ">
+                                    <div class="carousel-caption">
+                                        <h3><?php echo $value->title ?></h3>
+                                        <p><?php echo $value->intro ?></p>
+                                    </div>
+                                </div>
                             <?php } ?>
 
-                        </ul>
+                        </div>
 
+                        <!-- Left and right controls -->
+                        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                     </div>
+
+<!--                    <div class="box-links-right">-->
+<!--                        <p>-->
+<!--                            <span style="font-size:12px">-->
+<!--                                <span  style="font-family:tahoma,geneva,sans-serif"><strong>KHU VỰC&nbsp;HÀ NỘI</strong></span></span>-->
+<!--                        </p>-->
+<!---->
+<!--                        <ul>-->
+<!---->
+<!--                            --><?php //foreach ($news as $key=>$value){ ?>
+<!---->
+<!--                            <li>-->
+<!--                                <a href="--><?php //echo base_url('tin-tuc/'.create_slug($value->name).'-'.$value->id)?><!--">-->
+<!--                                    --><?php //echo $value->name?>
+<!--                                </a>-->
+<!--                            </li>-->
+<!---->
+<!--                            --><?php //} ?>
+<!---->
+<!--                        </ul>-->
+<!---->
+<!--                    </div>-->
                 </div>
             </div>
         </div>
