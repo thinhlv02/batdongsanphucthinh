69db77894b0f909211deaf6f345f63c8e59c19e9
thinhlv <thinhlv02@wru.vn>
2019-07-18 21:28:30 +0700

    udpate at home

--- application/views/site/home/ads.php
+++ application/views/site/home/ads.php
@@ -17,10 +17,10 @@
                                     <div class="img-news" style="background-image: url(<?php echo public_url('images/ads/' . $value->img) ?>)">
                                         <div class="content-news">
                                             <div>
-                                                <h6 style="color: #fff"><?php echo substr($value->title, 0 , 70) ?></h6>
+                                                <h6 style="color: #fff"><?php echo $value->title ?></h6>
                                             </div>
 <!--                                            <p style="color: #fff">--><?php //echo $value->intro ?><!--</p>-->
-                                            <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>" class="btn btn-primary mt-5">Xem thêm</a>
+<!--                                            <a href="--><?php //echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?><!--" class="btn btn-primary mt-5">Xem thêm</a>-->
                                         </div>
                                         <div class="title-news">
                                             <span style="font-size: 13px"><?php echo date('d/m/Y', strtotime($value->created_at)) ?></span><br>
