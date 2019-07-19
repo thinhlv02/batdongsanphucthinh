20c4ddd300f4ae5f50bac5fe0514d818e84f4707
thinhlv <thinhlv02@wru.vn>
2019-07-19 21:23:38 +0700

    udpate at home

--- application/views/site/pages/ads_detail.php
+++ application/views/site/pages/ads_detail.php
@@ -136,7 +136,7 @@
                         <div class="col-md-4 col-sm-6 col-xs-12">
                             <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
                                 <div class="item-news">
-                                    <div class="img-news" style="background-image: url(<?php echo public_url('images/news/' . $value->img) ?>)">
+                                    <div class="img-news" style="background-image: url(<?php echo public_url('images/ads/' . $value->img) ?>)">
                                         <div class="content-news">
                                             <div>
                                                 <strong style="color: #fff"><?php echo $value->title ?></strong>
