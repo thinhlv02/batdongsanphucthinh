deb4da40eb5dcb8d91ac40117f2e8afb088dec60
thinhlv <thinhlv02@wru.vn>
2019-07-20 16:42:32 +0700

    udpate at home

--- application/views/site/home/banner.php
+++ application/views/site/home/banner.php
@@ -87,9 +87,9 @@
 <!--                                    <img src="--><?php //echo public_url('images/ads/' . $value->img) ?><!--" alt="$value->img" style="width:100% ">-->
                                 <img src="<?php echo public_url('images/ads/' . $value->img) ?>" alt="<?php echo $value->img ?>" style="width:100%; height: 320px">
                                 <div class="carousel-caption">
-                                    <h3><a style="color: white !important;" href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
+                                    <h4><a style="color: white !important;" href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
                                             <?php echo $value->intro ?>
-                                        </a></h3>
+                                        </a></h4>
 <!--                                            <p><a class="text-danger" href="">-->
 <!--                                                --><?php //echo $value->intro ?>
 <!--                                            </a></p>-->
