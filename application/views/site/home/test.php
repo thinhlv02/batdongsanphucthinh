7127b27244725b5c7c7a7d5bf1070094a93bd257
thinh <thinhlv02@wru.vn>
2019-07-22 19:54:30 +0700

    vananh

--- application/views/site/home/test.php
+++ application/views/site/home/test.php
@@ -100,7 +100,7 @@
                                                 <!--                                <div class="box-img-thumb col-sm-3 col-md-3">-->
 
                                                 <div class="clearfix box-title-item">
-                                                    <label class="label label-danger">NỔI BẬT</label>
+<!--                                                    <label class="label label-danger">NỔI BẬT</label>-->
                                                     <h3 class="sieu-vip-title">
                                                         <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
                                                             <?php echo $value->title ?> </a>
@@ -290,7 +290,7 @@
                                                     </a>
                                                 </div>
                                                 <!--                                <div class="box-info-list col-sm-9 col-md-9">-->
-                                                <div class="box-info-list">
+                                                <div class="box-info-list" style="display: none">
                                                     <div class="clearfix box-title-item">
                                                         <label class="label label-danger">NỔI BẬT</label>
                                                         <h3 class="sieu-vip-title">
