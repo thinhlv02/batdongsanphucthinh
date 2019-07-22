314ed3468f57f9eea4254aebb198b769cc66b7b5
thinhlv <thinhlv02@wru.vn>
2019-07-22 23:36:55 +0700

    udpate at home

--- application/views/site/home/home.php
+++ application/views/site/home/home.php
@@ -6,7 +6,7 @@
 <?php //$this->load->view('site/home/service') ?>
 <?php //$this->load->view('site/home/intro_2') ?>
 <?php //$this->load->view('site/home/download') ?>
-<?php //$this->load->view('site/home/news')?>
+<?php $this->load->view('site/home/news')?>
 <?php //$this->load->view('site/home/product')?>
 <?php //$this->load->view('site/partner/partner')?>
 <?php //$this->load->view('site/recruitment')?>
