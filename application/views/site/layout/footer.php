14eadfe5af3d7ce5f61e39ac5c9c035b93e59e99
thinhlv <thinhluuvan@admicro.vn>
2019-07-25 17:26:40 +0700

    update

--- application/views/site/layout/footer.php
+++ application/views/site/layout/footer.php
@@ -4,15 +4,18 @@
             <div class="row">
                 <div class="card">
                     <div class="card-body" style="background-color: #ddd">
-                        <div class="col-md-3 col-sm-12 col-xs-12 mt-2">
+                        <div class="col-md-3 col-sm-12 col-xs-12 mt-2 text-center">
                             <!--                    <div class="row">-->
                             <!--                        <div class="col-md-12 col-sm-12 col-xs-12">-->
-                            <img src="<?php echo public_url('images/logo.png') ?>"
-                                 style="max-width: 180px; margin-top: 30px">
+
+                            <a href="<?php echo base_url() ?>">
+                                <img src="<?php echo public_url('images/logo.png') ?>" style="max-width: 180px; margin-top: 30px">
+                            </a>
+
                             <!--                        </div>-->
                             <!--                    </div>-->
                         </div>
-                        <div class="col-md-6 col-sm-12 col-xs-12 mt-2">
+                        <div class="col-md-3 col-sm-12 col-xs-12 mt-2">
                             <div class="footer-title"><strong>THÔNG TIN LIÊN HỆ</strong></div>
                             <div>
                                 <i class="fa fa-building" aria-hidden="true"></i>
@@ -27,7 +30,7 @@
                                 <span><?php echo $contact->email ?></span>
                             </div>
                         </div>
-                        <div class="col-md-3 col-sm-12 col-xs-12 mt-2">
+                        <div class="col-md-2 col-sm-12 col-xs-12 mt-2">
                             <div class="footer-title"><strong>LIÊN KẾT</strong></div>
                             <ul>
                                 <li><a href="<?php echo base_url('gioi-thieu-dich-vu') ?>">Giới thiệu dịch vụ</a></li>
@@ -36,6 +39,16 @@
                                 </li>
                                 <li><a href="<?php echo base_url('lien-he') ?>">Liên hệ</a></li>
                             </ul>
+                        </div>
+
+                        <div class="col-md-4 col-sm-12 col-xs-12 mt-2 text-center" style="">
+
+                            <a href="<?php echo base_url() ?>">
+                                <img src="<?php echo public_url('images/ads/default.png') ?>" style="max-width: 180px; margin-top: 30px">
+                            </a>
+
+<!--                            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbatdongsanphucthinhvn-1535407536595838%2F%3Fmodal%3Dadmin_todo_tour&tabs&width=340&height=214&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=166934463979079" width="340" height="214" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>-->
+
                         </div>
 
                         <div class="col-sm-12 col-md-12 col-xs-12 mt-2 text-center">
@@ -57,9 +70,9 @@
             <!--                    --><?php //}?>
             <!--                </div>-->
             <!--            </div>-->
-<!--            <div class="row col-sm-12 col-md-12 col-xs-12 mt-2 text-center">-->
-<!--                Copyright 2014 © phucthinh corp All Rights Reserved-->
-<!--            </div>-->
+            <!--            <div class="row col-sm-12 col-md-12 col-xs-12 mt-2 text-center">-->
+            <!--                Copyright 2014 © phucthinh corp All Rights Reserved-->
+            <!--            </div>-->
         </div>
 
     </div>
@@ -83,7 +96,9 @@
 
     //page up top
     // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
+    window.onscroll = function () {
+        scrollFunction()
+    };
 
     function scrollFunction() {
         if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
