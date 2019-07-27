162710f242df1d61c3a226e340bb317a096805b1
thinhlv <thinhlv02@wru.vn>
2019-07-27 12:50:08 +0700

    udpate at home

--- application/views/admin/ads/list.php
+++ application/views/admin/ads/list.php
@@ -46,7 +46,9 @@
                 <tr>
                     <td><?php echo $row->id?></td>
                     <td><img src="<?php echo base_url('public/images/ads/'.$row->img)?>" style="max-width: 80px"> </td>
-                    <td><?php echo $row->title?></td>
+                    <td>
+                        <a href="<?php echo base_url('rao-vat/' . create_slug($row->title) . '-' . $row->id) ?>" target="_blank"></a>
+                        <?php echo $row->title?></td>
 <!--                    <td>--><?php //echo $row->intro?><!--</td>-->
                     <td><?php echo $row->price?></td>
                     <td><?php echo $row->acreage?></td>
