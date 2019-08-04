de940ae1b4c5b89f203f598b23dd204246edd8a5
thinhlv <thinhlv02@wru.vn>
2019-08-04 17:58:53 +0700

    udpate at home

--- application/views/site/home/home.php
+++ application/views/site/home/home.php
@@ -11,5 +11,3 @@
 <?php //$this->load->view('site/home/product')?>
 <?php //$this->load->view('site/partner/partner')?>
 <?php //$this->load->view('site/recruitment')?>
-<?php //$this->load->view('site/recruitment')?>
-<?php //$this->load->view('site/recruitment')?>
