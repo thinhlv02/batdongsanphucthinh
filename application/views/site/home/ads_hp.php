b345c1a4525353a8f1c6aa812d43ad99bf1a1be7
thinhlv <thinhluuvan@admicro.vn>
2019-08-05 10:39:20 +0700

    update

--- application/views/site/home/ads_hp.php
+++ application/views/site/home/ads_hp.php
@@ -16,72 +16,58 @@
                         <p class="title_box"><strong>TIN GIÁ HẤP DẪN</strong></p>
 
                         <div class="row12 body_top2_box">
+
+                            <?php foreach ($ads_center as $key => $value) { ?>
                             <div class="col-xs-6 top2 top2_up">
                                 <div class="row12 _vip_hot">
                                     <div class="col-xs-5 ltop2">
-                                        <a href="/ban-nha-so-611418-mieu-hai-xa-le-chan-hai-phong-bds55367.htm"><img
-                                                    src="http://nhadathaiphong.vn/images/attachment/thumb/4949.jpg"
-                                                    alt="Bán nhà số 6/11/418 Miếu Hai Xã, Lê Chân, Hải Phòng"></a>
-                                        <div class="code_row">HP-55367</div>
+                                        <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>"><img
+                                                    src="<?php echo public_url('images/ads/' . $value->img) ?>"
+                                                    alt="<?php echo $value->title ?>"></a>
+                                        <div class="code_row">PT-<?php echo $value->id.substr($value->code,0,3) ?></div>
                                     </div>
 
                                     <div class="col-xs-7 rtop2">
                                         <h3>
-                                            <a href="/ban-nha-so-611418-mieu-hai-xa-le-chan-hai-phong-bds55367.htm">Bán
-                                                nhà số 6/11/418 Miếu Hai Xã, Lê Chân, Hải Phòng </a>
+                                            <a href="<?php echo base_url('rao-vat/' . create_slug($value->title) . '-' . $value->id) ?>">
+                                                <?php echo $value->title ?> </a>
                                             <span></span>
                                         </h3>
-                                        <div>Nhà xây 3,5 tầng độc lập, kiên cố, thiết kế hiện đại, ngõ rộng 2,5m,
-                                            gần
-                                            trường, chợ, dân cư đông đúc, hướng Đông Bắc, sổ đỏ chính chủ
+                                        <div><?php echo $value->intro ?>
                                         </div>
                                         <p>
-                                            <strong>DTMB:</strong> 48 m2 - <strong>Giá:</strong>
+                                            <strong>DTMB:</strong> <?php echo $value->acreage ?> m2 - <strong>Giá:</strong>
                                             <span>
-                                            1.89 tỷ VND												</span>
+                                           <?php echo $value->price ?> VND												</span>
                                         </p>
                                     </div>
                                 </div>
 
                                 <div class="icon_viphot">
-                                    <img src="<?php echo public_url('images/icon_new.gif') ?>"
-                                         alt="Bán nhà số 6/11/418 Miếu Hai Xã, Lê Chân, Hải Phòng"></div>
-                            </div>
-                            <div class="col-xs-6 top2 top2_up">
-                                <div class="row12 _vip_hot">
-                                    <div class="col-xs-5 ltop2">
-                                        <a href="/ban-nha-so-212-trung-hanh-hai-an-hai-phong-bds58634.htm"><img
-                                                    src="http://nhadathaiphong.vn/images/attachment/thumb/43651.jpg"
-                                                    alt="Bán nhà số 212 Trung Hành, Hải An, Hải Phòng"></a>
-                                        <div class="code_row">HP-58634</div>
-                                    </div>
 
-                                    <div class="col-xs-7 rtop2">
-                                        <h3>
-                                            <a href="/ban-nha-so-212-trung-hanh-hai-an-hai-phong-bds58634.htm">Bán
-                                                nhà
-                                                số 212 Trung Hành, Hải An, Hải Phòng </a>
-                                            <span></span>
-                                        </h3>
-                                        <div>Nhà xây 4 tầng kiên cố, nằm sau nhà mặt đường, ngõ rộng 3m, dân cư văn
-                                            minh, gần trường, chợ, bệnh viện, hướng Đông Nam - Tây Bắc, sổ đỏ chính
-                                            chủ
-                                        </div>
-                                        <p>
-                                            <strong>DTMB:</strong> 40 m2 - <strong>Giá:</strong>
-                                            <span>
-                                            1.7 tỷ VND												</span>
-                                        </p>
-                                    </div>
-                                </div>
+                                    <?php if ($value->icon_new == 1) { ?>
+                                        <img src="<?php echo public_url('images/icon_new.gif') ?>"
+                                             alt="<?php echo $value->title ?>">
 
-                                <div class="icon_viphot">
-                                    <img src="<?php echo public_url('images/icon_new.gif') ?>"
-                                         alt="Bán nhà số 212 Trung Hành, Hải An, Hải Phòng">
+                                    <?php } ?>
+
+                                    <?php if ($value->icon_vip == 1) { ?>
+                                        <img src="<?php echo public_url('images/icon_vip.gif') ?>"
+                                             alt="<?php echo $value->title ?>">
+
+                                    <?php } ?>
+
+                                    <?php if ($value->icon_hot == 1) { ?>
+
+                                        <img src="<?php echo public_url('images/icon_hot.gif') ?>"
+                                             alt="<?php echo $value->title ?>">
+                                    <?php } ?>
+
                                 </div>
                             </div>
 
-                            <div class="col-xs-6 top2 top2_up">
+                            <?php } ?>
+                            <div class="col-xs-6 top2 top2_up d-none">
                                 <div class="row12 _vip_hot">
                                     <div class="col-xs-5 ltop2">
                                         <a href="/ban-nha-so-212-trung-hanh-hai-an-hai-phong-bds58634.htm"><img
@@ -115,7 +101,41 @@
                                 </div>
                             </div>
 
-                            <div class="col-xs-6 top2 top2_up">
+                            <div class="col-xs-6 top2 top2_up d-none">
+                                <div class="row12 _vip_hot">
+                                    <div class="col-xs-5 ltop2">
+                                        <a href="/ban-nha-so-212-trung-hanh-hai-an-hai-phong-bds58634.htm"><img
+                                                    src="http://nhadathaiphong.vn/images/attachment/thumb/43651.jpg"
+                                                    alt="Bán nhà số 212 Trung Hành, Hải An, Hải Phòng"></a>
+                                        <div class="code_row">HP-58634</div>
+                                    </div>
+
+                                    <div class="col-xs-7 rtop2">
+                                        <h3>
+                                            <a href="/ban-nha-so-212-trung-hanh-hai-an-hai-phong-bds58634.htm">Bán
+                                                nhà
+                                                số 212 Trung Hành, Hải An, Hải Phòng </a>
+                                            <span></span>
+                                        </h3>
+                                        <div>Nhà xây 4 tầng kiên cố, nằm sau nhà mặt đường, ngõ rộng 3m, dân cư văn
+                                            minh, gần trường, chợ, bệnh viện, hướng Đông Nam - Tây Bắc, sổ đỏ chính
+                                            chủ
+                                        </div>
+                                        <p>
+                                            <strong>DTMB:</strong> 40 m2 - <strong>Giá:</strong>
+                                            <span>
+                                            1.7 tỷ VND												</span>
+                                        </p>
+                                    </div>
+                                </div>
+
+                                <div class="icon_viphot">
+                                    <img src="<?php echo public_url('images/icon_new.gif') ?>"
+                                         alt="Bán nhà số 212 Trung Hành, Hải An, Hải Phòng">
+                                </div>
+                            </div>
+
+                            <div class="col-xs-6 top2 top2_up d-none">
                                 <div class="row12 _vip_hot">
                                     <div class="col-xs-5 ltop2">
                                         <a href="/ban-nha-so-212-trung-hanh-hai-an-hai-phong-bds58634.htm"><img
@@ -162,10 +182,12 @@
                         <p class="title_box"><a href="/tin-vip.htm"><strong>TIN VIP</strong></a></p>
                         <div>
                             <div class="bx-wrapper" style="max-width: 100%; margin: 0px auto;">
-                                <div class="bx-viewport"
-                                     style="width: 100%; overflow: hidden; position: relative; height: 2520px;">
+<!--                                <div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative; height: 2520px;">-->
+                                <div class="bx-viewport" style="width: 100%; overflow: hidden; position: relative;">
                                     <ul class="vip_slider"
                                         style="width: auto; position: relative; transition-duration: 0s; transform: translate3d(0px, -2520px, 0px);">
+
+<!--                                        --><?php //foreach ($layer_right as $key => $value) { ?>
 
                                         <li style="float: none; list-style: none; position: relative; width: 298px;"
                                             class="bx-clone">
@@ -189,7 +211,10 @@
                                                 </dd>
                                             </dl>
                                         </li>
-                                        <li style="float: none; list-style: none; position: relative; width: 298px;"
+
+<!--                                        --><?php //} ?>
+
+                                        <li class="" style="float: none; list-style: none; position: relative; width: 298px;"
                                             class="bx-clone">
                                             <dl class="">
                                                 <dt>
@@ -210,7 +235,7 @@
                                                 </dd>
                                             </dl>
                                         </li>
-                                        <li style="float: none; list-style: none; position: relative; width: 298px;"
+                                        <li class=""  style="float: none; list-style: none; position: relative; width: 298px;"
                                             class="bx-clone">
                                             <dl class=" _vip">
                                                 <dt>
@@ -232,7 +257,7 @@
                                                 </dd>
                                             </dl>
                                         </li>
-                                        <li style="float: none; list-style: none; position: relative; width: 298px;"
+                                        <li class=""  style="float: none; list-style: none; position: relative; width: 298px;"
                                             class="bx-clone">
                                             <dl class="">
                                                 <dt>
@@ -266,64 +291,7 @@
                         </div>
                     </div>
                     <!--Begin two_cols-->
-                    <div class="adv_box">
-                        <a href="http://nhadathaiphong.vn/chinh-chu-can-ban-dat-tai-dinh-cu-xi-mang---so-1-lo-34-so-dau-hong-bang-hai-phong-bds49141.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/2895số 1 lô 34.jpg"
-                                    alt="BÁN ĐẤT SỐ 1 LÔ 34 TĐC XI MĂNG"></a><a
-                                href="http://nhadathaiphong.vn/ban-can-shophouse-2-mat-tien-so-10-khu-cho-dau-moi-hoa-quatai-dinh-cu-xi-mang-so-dau-hong-bang-hai-phong-bds46569.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/3968KIOT-HOA QUA-banner.jpg"
-                                    alt="BÁN KIOT CHỢ ĐẦU MỐI HOA QUẢ"></a><a
-                                href="http://nhadathaiphong.vn/ban-1100m-dat-lo-18-le-hong-phong-hai-an-hai-phong-bds46840.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/2110bandatlehongphong.jpg"
-                                    alt="BÁN ĐẤT LÔ 18 LÊ HỒNG PHONG"></a><a
-                                href="http://nhadathaiphong.vn/ban-dat-nen-du-an-me-linh---duong-kinh-hai-phong-bds47026.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/8927tk-du-an-me-linh-duong-kinh-phan.jpg"
-                                    alt="DỰ ÁN MÊ LINH - DƯƠNG KINH (A.HÙNG)"></a><a
-                                href="http://nhadathaiphong.vn/ban-tin-nha-dat/vinhomes-marina---khu-do-thi-sinh-thai-dang-song-tai-hai-phong-n232.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/9528vinhomes-marina-5.gif"
-                                    alt="VINHOMES MARINA 2"></a><a
-                                href="http://nhadathaiphong.vn/dien-dan--hoi-dap/bang-gia-dich-vu-quang-cao-n179.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/9635tk-chuyen-vien-tu-van-bds-5.jpg"
-                                    alt="CHUYÊN VIÊN TƯ VẤN"></a><a
-                                href="http://nhadatlehongphong.vn/nha-moi-gioi/pham-van-kien-m18.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/6472tu-van-phan-phoi-kien.gif"
-                                    alt="Phạm Kiên"></a><a
-                                href="http://vincityhaiphong.net/"><img
-                                    src="http://nhadathaiphong.vn/images/partner/6100the_monaco_vinhomes_imperia_hai_phong_vincityhaiphong.jpg"
-                                    alt="VINCITYHAIPHONG.NET"></a><a
-                                href="http://catbacity.vn/khach-san-hoi-an/du-lich-cat-ba-nhung-dieu-ban-nen-biet-n25.html"><img
-                                    src="http://nhadathaiphong.vn/images/partner/1825z4.jpg" alt="CATBACITY.VN"></a><a
-                                href="http://nhadatcatba.vn/"><img
-                                    src="http://nhadathaiphong.vn/images/partner/573thiet-ke-banner-cat-ba-ben-phai.gif"
-                                    alt="NHÀ ĐẤT CÁT BÀ"></a><a
-                                href="http://nhadathaiphong.vn/ban-gap-can-ho-so-107108-toa-b1-khu-chung-cu-bac-son-kien-an-hai-phong-bds20167.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/2540Ban-chung-cu-bac-son-jpg1.jpg"
-                                    alt="BÁN GẤP CĂN HỘ SỐ 107+108 TÒA B1 KHU CHUNG CƯ BẮC SƠN, KIẾN AN, HẢI PHÒNG"></a><a
-                                href="http://nhadathaiphong.vn/ban-tin-nha-dat/khu-do-thi-hoang-huy-riverside-n214.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/7973HOANG-HUY-RIVERSIDE.jpg"
-                                    alt="HOÀNG HUY SÔNG CẤM"></a><a
-                                href="http://nhadathaiphong.vn/tim-kiem.htm?txtkeyword=CHO+THU%C3%8A"><img
-                                    src="http://nhadathaiphong.vn/images/partner/6840cho-thue-nha-mat-pho-trung-tam.gif"
-                                    alt="CHO THUÊ NHÀ MẶT PHỐ"></a><a
-                                href="http://nhadathaiphong.vn/dien-dan--hoi-dap/bang-gia-dich-vu-quang-cao-n179.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/9803quang-cao-banner-2019-3.gif"
-                                    alt="QUẢNG CÁO BANNER"></a><a
-                                href="http://nhadathaiphong.vn/so-huu-can-ho-4-chuan-nhat-ban-ngay-mat-tien-lach-tray-voi-uu-dai-tot-nhat-bds3839.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/3142SHP.gif" alt="SHP"></a><a href="#"><img
-                                    src="http://nhadathaiphong.vn/images/partner/3320ĐẤT HỒNG BÀNG.jpg"
-                                    alt="CHUYỂN NHƯỢNG ĐẤT SAU TTHC QUẬN HỒNG BÀNG"></a><a
-                                href="http://nhadathaiphong.vn/tim-kiem.htm?txtkeyword=ph%C3%BAc+l%E1%BB%99c"><img
-                                    src="http://nhadathaiphong.vn/images/partner/9235PHAN-PHOI-DAT-NEN-DU-AN.jpg"
-                                    alt="DỰ ÁN PHÚC LỘC - 2"></a><a
-                                href="http://nhadathaiphong.vn/ban-tin-nha-dat/flamingo-cat-ba-beach-resort---ban-giao-huong-bon-mua-khuc-ca-tuyet-voi-giao-hoa-troi-va-bien-n205.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/6051Flamingo-Catba.gif"
-                                    alt="FLAMINGO CATBA"></a><a
-                                href="http://nhadathaiphong.vn/kinh-nghiem-mua-ban-nha-dat/tan-mat-chung-kien-nha-mau-du-an-pruksa-town-dep-nhu-trong-tranh-n196.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/3286pruksa-town-hoang-huy-so-hong-lau-dai.gif"
-                                    alt="PRUKSA-TOWN"></a><a
-                                href="http://nhadathaiphong.vn/ban-tin-nha-dat/flamingo-cat-ba-beach-resort---ban-giao-huong-bon-mua-khuc-ca-tuyet-voi-giao-hoa-troi-va-bien-n205.htm"><img
-                                    src="http://nhadathaiphong.vn/images/partner/7085Flamingo-Catba-2.gif"
-                                    alt="FLAMINGO CATBA - 2"></a></div>
+
                 </div>
             </div>
 
