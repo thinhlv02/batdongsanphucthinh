49fad2adb40b2e8e2f4c74e9d405f0947a83209e
thinhlv <thinhluuvan@admicro.vn>
2019-08-06 13:49:20 +0700

    update

--- application/views/site/pages/service_info.php
+++ application/views/site/pages/service_info.php
@@ -1,28 +1,37 @@
-
-
 <section class="detail">
     <div class="container">
         <div class="col-sm-4 col-md-3">
             <div class="left-menu">
                 <div class="left-title">Dịch vụ</div>
                 <ul>
-                    <?php foreach ($products as $p){ ?>
+                    <?php foreach ($products as $p) { ?>
                         <li>
-                            <a href="<?php echo base_url('gioi-thieu-dich-vu/'.create_slug($p->name).'-'.$p->id.'.html')?>">
-                                <?php echo $p->name?>
+                            <a href="<?php echo base_url('gioi-thieu-dich-vu/' . create_slug($p->name) . '-' . $p->id . '.html') ?>">
+                                <?php echo $p->name ?>
                             </a>
                         </li>
-                    <?php }?>
+                    <?php } ?>
                 </ul>
             </div>
         </div>
         <div class="col-md-9 col-sm-8 detail-content">
-            <?php if(isset($product)){ ?>
-                <h2><?php echo $product->name?></h2>
-                <p>
-                    <?php echo $product->content?>
-                </p>
-            <?php }?>
+            <div class="card">
+                <div class="card-body">
+
+
+                    <?php if (isset($product)) { ?>
+                        <h2><?php echo $product->name ?></h2>
+                        <p>
+                            <?php echo $product->content ?>
+                        </p>
+                    <?php } ?>
+
+                </div>
+
+            </div>
+
         </div>
+
     </div>
+
 </section>
