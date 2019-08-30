3119ab46b8883432ee21e39152959363e729d33d
thinhlv <thinhluuvan@admicro.vn>
2019-08-30 17:23:25 +0700

    update

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -32,7 +32,7 @@
 </div>
 
 <div class="row">
-    <div class="col-md-6 col-sm-6 col-xs-6">
+    <div class="col-md-7 col-sm-7 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Thông tin ngân hàng chuyển tiền
@@ -114,7 +114,7 @@
         </div>
     </div>
 
-    <div class="col-md-6 col-sm-6 col-xs-12">
+    <div class="col-md-5 col-sm-5 col-xs-12">
         <div class="x_panel">
             <div class="x_title">
                 <h2>Thông tin tài khoản facebook cho cskh</h2>
