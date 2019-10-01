244c76839b1366e4fc285ae3e68d88b87a888087
thinhlv <thinhlv02@wru.vn>
2019-10-01 22:41:45 +0700

    udpate at home

--- application/views/site/layout/banner.php
+++ application/views/site/layout/banner.php
@@ -4,9 +4,14 @@
 
 <section class="pb-0">
     <div class="container banner" style="background-image: url(<?php echo public_url('images/' . $content->banner) ?>);background-size: 100%;">
+        <div class="" style="position: absolute;top: 2px;left: 2px;color: #ffffff;">
+            <marquee>Bất động sản phúc thịnh - Đơn giản hóa việc bán hàng - Vì chúng tôi luôn bên bạn !</marquee>
+
+
+        </div>
         <div class="banner-left">
 <!--            <p style="display: none" class="caption-banner animated slideInLeft">Đơn giản hoá việc bán hàng<br>vì chúng tôi luôn bên bạn-->
-            <p class="caption-banner animated slideInLeft">Đơn giản hoá việc bán hàng<br>vì chúng tôi luôn bên bạn
+            <p class="caption-banner animated slideInLeft d-none">Đơn giản hoá việc bán hàng<br>vì chúng tôi luôn bên bạn
 <!--            <p class="caption-banner animated slideInLeft">Đơn giản hoá việc bán hàng , vì chúng tôi luôn bên bạn-->
             </p>
             <!--        <a href="--><?php //echo base_url('gioi-thieu-dich-vu.html')?><!--"-->
--- application/views/site/layout/menu.php
+++ application/views/site/layout/menu.php
@@ -1,5 +1,6 @@
 
 <div class="sub-nav">
+    <div id="hot_line">Hotline: 0796 43 22 11</div>
     <div class="logo">
         <a href="<?php echo base_url() ?>">
 
@@ -8,12 +9,12 @@
 <!--        <a href="--><?php //echo base_url()?><!--">Trao tiện ích, nhận tin yêu</a>-->
     </div>
     <ul class="ul-large">
-        <li class="<?php echo isset($li_1) ? 'menu-active' : ''?>" title="Trang chủ"><a href="<?php echo base_url()?>">Trang chủ</a></li>
-        <li class="<?php echo isset($li_2) ? 'menu-active' : ''?>" title="Giới thiệu dịch vụ"><a href="<?php echo base_url('gioi-thieu-dich-vu')?>">Giới thiệu dịch vụ</a></li>
-        <li class="<?php echo isset($li_3) ? 'menu-active' : ''?>" title="Hỗ trợ"><a href="<?php echo base_url('ho-tro')?>">Hỗ trợ</a></li>
-        <li class="<?php echo isset($li_6) ? 'menu-active' : ''?>" title="Tin tức"><a href="<?php echo base_url('tin-tuc')?>">Tin tức</a></li>
-        <li class="<?php echo isset($li_4) ? 'menu-active' : ''?>" title="Chính sách và điều khoản"><a href="<?php echo base_url('dieu-khoan-su-dung')?>">Chính sách và điều khoản</a></li>
-        <li class="<?php echo isset($li_5) ? 'menu-active' : ''?>" title="Liên hệ"><a href="<?php echo base_url('lien-he')?>">Liên hệ</a></li>
+        <li class="<?php echo isset($li_1) ? 'menu-active' : ''?>" title="Trang chủ"><a href="<?php echo base_url()?>"><i class="fa fa-home"></i> Trang chủ</a></li>
+        <li class="<?php echo isset($li_2) ? 'menu-active' : ''?>" title="Giới thiệu dịch vụ"><a href="<?php echo base_url('gioi-thieu-dich-vu')?>"><i class="fa fa-window-restore"></i> Giới thiệu dịch vụ</a></li>
+        <li class="<?php echo isset($li_3) ? 'menu-active' : ''?>" title="Hỗ trợ"><a href="<?php echo base_url('ho-tro')?>"><i class="fa fa-life-ring"></i> Hỗ trợ</a></li>
+        <li class="<?php echo isset($li_6) ? 'menu-active' : ''?>" title="Tin tức"><a href="<?php echo base_url('tin-tuc')?>"><i class="fa fa-newspaper"></i> Tin tức</a></li>
+        <li class="<?php echo isset($li_4) ? 'menu-active' : ''?>" title="Chính sách và điều khoản"><a href="<?php echo base_url('dieu-khoan-su-dung')?>"><i class="fa fa-handshake"></i> Chính sách và điều khoản</a></li>
+        <li class="<?php echo isset($li_5) ? 'menu-active' : ''?>" title="Liên hệ"><a href="<?php echo base_url('lien-he')?>"><i class="fa fa-user-md"></i> Liên hệ</a></li>
     </ul>
 
 
