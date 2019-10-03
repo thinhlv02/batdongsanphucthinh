62e48660349d40524f04d252414bd3e3497581f3
thinhlv <thinhluuvan@admicro.vn>
2019-10-03 11:59:58 +0700

    update

--- application/views/site/layout/menu.php
+++ application/views/site/layout/menu.php
@@ -10,19 +10,19 @@
     </div>
 <!--    switch language -->
     <div id="language">
-        <a href="<?php echo $language == 'vn' ? base_url('en' .getLastUri()) : base_url('vn'.getLastUri()) ?>">
+        <a href="<?php echo $language == 'vn' ? base_url('en/' .getLastUri()) : base_url('vn/'.getLastUri()) ?>">
             <img src="<?php echo $language == 'vn' ? public_url('images/flag_us.png') : public_url('images/flag_vn.png') ?>"/>
             <?php echo $language == 'vn' ? 'English' : 'Vietnamese' ; ?>
         </a>
     </div>
 
     <ul class="ul-large">
-        <li class="<?php echo isset($li_1) ? 'menu-active' : ''?>" title="Trang chủ"><a href="<?php echo base_url()?>"><i class="fa fa-home"></i> Trang chủ</a></li>
-        <li class="<?php echo isset($li_2) ? 'menu-active' : ''?>" title="Giới thiệu dịch vụ"><a href="<?php echo base_url('gioi-thieu-dich-vu')?>"><i class="fa fa-window-restore"></i> Giới thiệu dịch vụ</a></li>
-        <li class="<?php echo isset($li_3) ? 'menu-active' : ''?>" title="Hỗ trợ"><a href="<?php echo base_url('ho-tro')?>"><i class="fa fa-life-ring"></i> Hỗ trợ</a></li>
-        <li class="<?php echo isset($li_6) ? 'menu-active' : ''?>" title="Tin tức"><a href="<?php echo base_url('tin-tuc')?>"><i class="fa fa-newspaper"></i> Tin tức</a></li>
-        <li class="<?php echo isset($li_4) ? 'menu-active' : ''?>" title="Chính sách và điều khoản"><a href="<?php echo base_url('dieu-khoan-su-dung')?>"><i class="fa fa-handshake"></i> Chính sách và điều khoản</a></li>
-        <li class="<?php echo isset($li_5) ? 'menu-active' : ''?>" title="Liên hệ"><a href="<?php echo base_url('lien-he')?>"><i class="fa fa-user-md"></i> Liên hệ</a></li>
+        <li class="<?php echo isset($li_1) ? 'menu-active' : ''?>" title="Trang chủ"><a href="<?php echo base_url()?>"><i class="fa fa-home"></i> <?php echo $this->lang->line('home'); ?></a></li>
+        <li class="<?php echo isset($li_2) ? 'menu-active' : ''?>" title="Giới thiệu dịch vụ"><a href="<?php echo base_url('gioi-thieu-dich-vu')?>"><i class="fa fa-window-restore"></i> <?php echo $this->lang->line('intro'); ?></a></li>
+        <li class="<?php echo isset($li_3) ? 'menu-active' : ''?>" title="Hỗ trợ"><a href="<?php echo base_url('ho-tro')?>"><i class="fa fa-life-ring"></i> <?php echo $this->lang->line('support'); ?></a></li>
+        <li class="<?php echo isset($li_6) ? 'menu-active' : ''?>" title="Tin tức"><a href="<?php echo base_url('tin-tuc')?>"><i class="fa fa-newspaper"></i> <?php echo $this->lang->line('news_menu'); ?></a></li>
+        <li class="<?php echo isset($li_4) ? 'menu-active' : ''?>" title="Chính sách và điều khoản"><a href="<?php echo base_url('dieu-khoan-su-dung')?>"><i class="fa fa-handshake"></i> <?php echo $this->lang->line('policies'); ?></a></li>
+        <li class="<?php echo isset($li_5) ? 'menu-active' : ''?>" title="Liên hệ"><a href="<?php echo base_url('lien-he')?>"><i class="fa fa-user-md"></i> <?php echo $this->lang->line('contact'); ?></a></li>
     </ul>
 
     <nav role='navigation' class="nav-small">
@@ -32,12 +32,12 @@
             <span></span>
             <span></span>
             <ul id="menu">
-                <a href="<?php echo base_url()?>"><li>Trang chủ</li></a>
-                <a href="<?php echo base_url('gioi-thieu-dich-vu')?>"><li>Giới thiệu dịch vụ</li></a>
-                <a href="<?php echo base_url('ho-tro')?>"><li>Hỗ trợ</li></a>
-                <a href="<?php echo base_url('tin-tuc')?>"><li>Tin tức</li></a>
-                <a href="<?php echo base_url('dieu-khoan-su-dung')?>"><li>Chính sách và điều khoản</li></a>
-                <a href="<?php echo base_url('lien-he')?>"><li>Liên hệ</li></a>
+                <a href="<?php echo base_url()?>"><li><?php echo $this->lang->line('home'); ?></li></a>
+                <a href="<?php echo base_url('gioi-thieu-dich-vu')?>"><li><?php echo $this->lang->line('intro'); ?></li></a>
+                <a href="<?php echo base_url('ho-tro')?>"><li><?php echo $this->lang->line('support'); ?></li></a>
+                <a href="<?php echo base_url('tin-tuc')?>"><li><?php echo $this->lang->line('news_menu'); ?></li></a>
+                <a href="<?php echo base_url('dieu-khoan-su-dung')?>"><li><?php echo $this->lang->line('policies'); ?></li></a>
+                <a href="<?php echo base_url('lien-he')?>"><li><?php echo $this->lang->line('contact'); ?></li></a>
             </ul>
         </div>
     </nav>
