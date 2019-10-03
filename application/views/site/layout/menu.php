b4281ced5a68f364d310f09324d3688ee96dded7
thinhlv <thinhlv02@wru.vn>
2019-10-03 22:47:06 +0700

    udpate at home

--- application/views/site/layout/menu.php
+++ application/views/site/layout/menu.php
@@ -17,12 +17,14 @@
     </div>
 
     <ul class="ul-large">
-        <li class="<?php echo isset($li_1) ? 'menu-active' : ''?>" title="Trang chủ"><a href="<?php echo base_url()?>"><i class="fa fa-home"></i> <?php echo $this->lang->line('home'); ?></a></li>
-        <li class="<?php echo isset($li_2) ? 'menu-active' : ''?>" title="Giới thiệu dịch vụ"><a href="<?php echo base_url('gioi-thieu-dich-vu')?>"><i class="fa fa-window-restore"></i> <?php echo $this->lang->line('intro'); ?></a></li>
-        <li class="<?php echo isset($li_3) ? 'menu-active' : ''?>" title="Hỗ trợ"><a href="<?php echo base_url('ho-tro')?>"><i class="fa fa-life-ring"></i> <?php echo $this->lang->line('support'); ?></a></li>
-        <li class="<?php echo isset($li_6) ? 'menu-active' : ''?>" title="Tin tức"><a href="<?php echo base_url('tin-tuc')?>"><i class="fa fa-newspaper"></i> <?php echo $this->lang->line('news_menu'); ?></a></li>
-        <li class="<?php echo isset($li_4) ? 'menu-active' : ''?>" title="Chính sách và điều khoản"><a href="<?php echo base_url('dieu-khoan-su-dung')?>"><i class="fa fa-handshake"></i> <?php echo $this->lang->line('policies'); ?></a></li>
-        <li class="<?php echo isset($li_5) ? 'menu-active' : ''?>" title="Liên hệ"><a href="<?php echo base_url('lien-he')?>"><i class="fa fa-user-md"></i> <?php echo $this->lang->line('contact'); ?></a></li>
+        <li class="<?php echo isset($li_1) ? 'menu-active' : ''?>" title="Trang chủ">
+            <a href="<?php echo base_url()?>"><i class="fa fa-home"></i> <?php echo $this->lang->line('home'); ?></a>
+        </li>
+        <li class="<?php echo isset($li_2) ? 'menu-active' : ''?>" title="Giới thiệu dịch vụ"><a href="<?php echo base_url('gioi-thieu-dich-vu')?>"> <?php echo $this->lang->line('intro'); ?></a></li>
+        <li class="<?php echo isset($li_3) ? 'menu-active' : ''?>" title="Hỗ trợ"><a href="<?php echo base_url('ho-tro')?>"> <?php echo $this->lang->line('support'); ?></a></li>
+        <li class="<?php echo isset($li_6) ? 'menu-active' : ''?>" title="Tin tức"><a href="<?php echo base_url('tin-tuc')?>"> <?php echo $this->lang->line('news_menu'); ?></a></li>
+        <li class="<?php echo isset($li_4) ? 'menu-active' : ''?>" title="Chính sách và điều khoản"><a href="<?php echo base_url('dieu-khoan-su-dung')?>"> <?php echo $this->lang->line('policies'); ?></a></li>
+        <li class="<?php echo isset($li_5) ? 'menu-active' : ''?>" title="Liên hệ"><a href="<?php echo base_url('lien-he')?>"> <?php echo $this->lang->line('contact'); ?></a></li>
 <!--        register - login-->
         <li class="user_style" title="<?php echo $this->lang->line('login'); ?>">
             <a href="javascript:void(0)" class="menu_login">
@@ -56,3 +58,11 @@
     </nav>
 
 </div>
+
+<script>
+    $(document).ready(function(){
+        $('.menu-active').each(function() {
+            $(this).prepend('<span></span>');
+        });
+    });
+</script>
