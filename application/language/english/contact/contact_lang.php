0a5045a623c0c1b176af4bfa94b5821856baaa9a
thinhlv <thinhluuvan@admicro.vn>
2019-10-10 10:49:03 +0700

    update

--- application/language/english/contact/contact_lang.php
+++ application/language/english/contact/contact_lang.php
@@ -1,3 +1,3 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 $lang['contact_lang'] = array();
-$lang['contact_lang']['login_title'] = 'Contact Us!';
+$lang['contact_lang']['title'] = 'Contact Us!';
--- application/language/vietnamese/contact/contact_lang.php
+++ application/language/vietnamese/contact/contact_lang.php
@@ -1,3 +1,3 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 $lang['contact_lang'] = array();
-$lang['contact_lang']['login_title'] = 'Liên hệ với chúng tôi!';
+$lang['contact_lang']['title'] = 'Liên hệ với chúng tôi!';
