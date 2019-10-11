23f653ef9a655560b14174bf30a6fdea91f03bff
thinhlv <thinhluuvan@admicro.vn>
2019-10-11 10:04:43 +0700

    update

--- application/views/site/home/form_search.php
+++ application/views/site/home/form_search.php
@@ -1,26 +1,18 @@
-
 <div class="col-sm-2 col-md-2">
     <div class="box-module mt-2">
-        <div class="bg-modul"><h7>
-                <i class="glyphicon glyphicon-search"></i> <?php echo $common_lang['search_title']; ?></h7></div>
+        <div class="bg-modul w-100 text-center">
+            <h7>
+                <i class="glyphicon glyphicon-search"></i> <?php echo $common_lang['search_title']; ?></h7>
+        </div>
     </div>
-    <div class="card" style="background: #5caceb33 !important;">
+    <div class="card" style="border: 1px solid #cacfe7;">
         <div class="card-body pt-0 " style="height: 295px;">
-            <!--                        <div class="box-module">-->
-            <!--                            <div class="bg-modul"><i class="glyphicon glyphicon-search"></i> Tìm kiếm</div>-->
-            <!--                        </div>-->
-            <!--                        <div class="page-title text-center">-->
-            <!--                            <h4 class="border-bottom">Chức năng tìm kiếm</h4>-->
-            <!--                            <button type="button" class="btn btn-success">Thanh tìm kiếm <span class="badge"></span></button>-->
-            <!--                        </div>-->
-
-            <!--                        <form id="formAddProduct_book" method="post" enctype="multipart/form-data">-->
 
             <form id="formAddProduct_book12" data-parsley-validate class="" method="get"
                   action="<?php echo base_url('tim-kiem') ?>"
                   enctype="multipart/form-data">
-                <div class="form-group">
-<!--                    <label for="email">Mã tin:</label>-->
+                <div class="form-group mt-3">
+                    <!--                    <label for="email">Mã tin:</label>-->
                     <input type="text" class="form-control" placeholder="<?php echo $common_lang['search_phone']; ?>" name="code">
                 </div>
                 <div class="form-group">
@@ -54,7 +46,7 @@
                 </div>
 
                 <div class="form-group">
-<!--                                            <label for="email">Loại</label>-->
+                    <!--                                            <label for="email">Loại</label>-->
                     <select class="form-control">
                         <option value=""> -- <?php echo $common_lang['choose_type']; ?> --</option>
                         <option value="1"> <?php echo $common_lang['for_sale']; ?></option>
@@ -62,8 +54,7 @@
                     </select>
                 </div>
                 <div class="text-center">
-
-                    <button style="background: #5caceb " type="submit" class="btn btn-default"><?php echo $common_lang['btn_search']; ?>
+                    <button style="background-color: #2196F3;color: #fff;" type="submit" class="btn btn-default w-100"><i class="glyphicon glyphicon-search"></i> <?php echo $common_lang['btn_search']; ?>
                     </button>
                 </div>
 
