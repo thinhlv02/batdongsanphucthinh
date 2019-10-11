d978857b2109961e9a5e32e657a82e41aaf67e8e
thinhlv <thinhluuvan@admicro.vn>
2019-10-11 17:39:10 +0700

    update

--- application/views/site/pages/user_page.php
+++ application/views/site/pages/user_page.php
@@ -1,22 +1,36 @@
 <div class="container">
     <div class="title-section"><h2><?php echo $user_page_lang['title']; ?></h2></div>
-    <div class="row" style="margin-top: 30px">
-        <?php foreach ($agencies as $key => $value) { ?>
-            <div class="item-agency" id="<?php echo create_slug($value->name) ?>">
-                <div style="font-weight: bold"><?php echo $value->name ?></div>
-                <div style="color: #666"><i class="fa fa-building" aria-hidden="true"></i>
-                    <span><?php echo $value->address ?></span></div>
-                <div style="color: #666"><i class="fa fa-phone" aria-hidden="true"></i>
-                    <span><?php echo $value->phone ?></span></div>
+    <div class="row mt-5">
+        <div class="col-sm-4 col-md-3">
+            <div class="left-menu">
+                <div class="left-title">Thông tin cá nhân</div>
+
+
+                <h2 style="text-align:center">User Profile Card</h2>
+
+                <div class="card text-center">
+                    <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" style="width:100%">
+                    <h1>John Doe</h1>
+                    <p class="title">CEO & Founder, Example</p>
+                    <p>Harvard University</p>
+                    <div style="margin: 24px 0;">
+                        <a href="#"><i class="fa fa-dribbble"></i></a>
+                        <a href="#"><i class="fa fa-twitter"></i></a>
+                        <a href="#"><i class="fa fa-linkedin"></i></a>
+                        <a href="#"><i class="fa fa-facebook"></i></a>
+                    </div>
+                    <p>
+                        <button>Contact</button>
+                    </p>
+                </div>
             </div>
-        <?php } ?>
+        </div>
+
+        <div class="col-md-9 col-sm-8 detail-content">
+            <div class="line-height-2">
+                <div class="left-title">Danh sách bài đăng !</div>
+
+            </div>
+        </div>
     </div>
 </div>
-
-<script>
-    $(document).ready(function () {
-        if (window.location.href.includes("div-panel")) {
-            $(document).scrollTop(450);
-        }
-    });
-</script>
