0aa64351f265f7527ec7ef847252f2363d01c8fa
thinhlv <thinhluuvan@admicro.vn>
2019-10-11 17:45:50 +0700

    update

--- application/views/site/pages/user_page.php
+++ application/views/site/pages/user_page.php
@@ -3,21 +3,21 @@
     <div class="row mt-5">
         <div class="col-sm-4 col-md-3">
             <div class="left-menu">
-                <div class="left-title">Thông tin cá nhân</div>
+                <div class="left-title text-center">Thông tin cá nhân</div>
 
 
                 <h2 style="text-align:center">User Profile Card</h2>
 
                 <div class="card text-center">
                     <img src="https://www.w3schools.com/w3images/team2.jpg" alt="John" style="width:100%">
-                    <h1>John Doe</h1>
+                    <h1>Khách hàng 1</h1>
                     <p class="title">CEO & Founder, Example</p>
                     <p>Harvard University</p>
                     <div style="margin: 24px 0;">
-                        <a href="#"><i class="fa fa-dribbble"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-linkedin"></i></a>
-                        <a href="#"><i class="fa fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-dribbble fa-lg"></i></a>
+                        <a href="#"><i class="fab fa-twitter fa-lg"></i></a>
+                        <a href="#"><i class="fab fa-linkedin fa-lg"></i></a>
+                        <a href="#"><i class="fab fa-facebook fa-lg"></i></a>
                     </div>
                     <p>
                         <button>Contact</button>
