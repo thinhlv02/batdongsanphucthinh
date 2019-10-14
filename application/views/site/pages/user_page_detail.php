7b6980b5e672cca0afb42cee3c95b30d305abb01
thinhlv <thinhluuvan@admicro.vn>
2019-10-14 15:06:43 +0700

    update

--- application/views/site/pages/user_page_detail.php
+++ application/views/site/pages/user_page_detail.php
@@ -1,16 +1,16 @@
 <div class="container">
-    <div class="row mt-5">
+    <div class="row mt-3">
         <div class="col-sm-12 col-md-12 detail-content">
             <p class="title_box" style="color: #055699"><strong>Thông tin chi tiết
                     <i class="fa fa-angle-right"></i> <?php echo $ads->title ?></strong>
             </p>
             <div class="card">
-                <div class="card-header">Link web</div>
+                <div class="card-header text-center text-uppercase text-bold-600">Link web</div>
                 <div class="card-body">
                     <?php echo $ads->link_web; ?>
                 </div>
 
-                <div class="card-header">Link facebook</div>
+                <div class="card-header text-center text-uppercase text-bold-600">Link facebook</div>
                 <div class="card-body">
                     <?php echo $ads->link_fb; ?>
                 </div>
