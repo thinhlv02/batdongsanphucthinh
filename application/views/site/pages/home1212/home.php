1b4a22edb3de53b48f5226b09cf52f8e7ccba0ac
thinhlv <thinhluuvan@admicro.vn>
2019-10-15 09:31:20 +0700

    update

--- application/views/site/pages/home1212/home.php
+++
@@ -1,8 +0,0 @@
-<?php $this->load->view($this->_template_f . 'home/intro_1') ?>
-<?php $this->load->view($this->_template_f . 'home/service') ?>
-<?php $this->load->view($this->_template_f . 'home/intro_2') ?>
-<?php $this->load->view($this->_template_f . 'home/download') ?>
-<?php $this->load->view($this->_template_f . 'home/news')?>
-<?php //$this->load->view($this->_template_f . 'home/product')?>
-<?php //$this->load->view($this->_template_f . 'partner/partner')?>
-<?php //$this->load->view($this->_template_f . 'recruitment')?>
