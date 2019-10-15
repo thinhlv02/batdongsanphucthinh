c20725fb0e3a5073c4ec65b310f756898cb46ada
thinhlv <thinhluuvan@admicro.vn>
2019-10-15 14:57:32 +0700

    update

--- application/views/site/pages/price_view.php
+++ application/views/site/pages/price_view.php
@@ -5,7 +5,7 @@
                 <div class="left-title">Bảng giá quảng cáo</div>
                 <ul>
                     <?php foreach ($prices as $k => $p) { ?>
-                        <li class="<?php if ($p->id == $active || ($k == 0 && $active == 0)) echo 'active'; ?>" onclick="showHeaderPrice('<?php echo $p->id; ?>')">
+                        <li class="<?php if ($p->id == $active || ($k == 0 && $active == 0)) echo 'active'; ?>" onclick="showTitlePrice('<?php echo $p->id; ?>')">
                             <a href="<?php echo base_url('bang-gia/' . create_slug($p->name) . '-' . $p->id . '.html#' . $p->id) ?>">
                                 <?php echo $p->name ?>
                             </a>
@@ -18,9 +18,8 @@
             <div class="line-height-2 pl-5">
 
                 <?php if (isset($price)) { ?>
-                    <!--                    <h2>--><?php //echo $price->name ?><!--</h2>-->
                     <div style="border: 1px solid #ccc; border-radius: 8px 8px 0 0; padding: 10px 0;">
-                        <div id="pad1" class="padcontent" style="display: block;">
+                        <div id="price1" class="priceTitle" style="display: block;">
                             <div style="color: #055699; font-weight: bold;">
                                 <div style="width: 350px; float: left; margin: 80px 0 0 60px;">
                                     <div style="width: 80px; float: left; font-size: 30px; line-height: 35px; font-family: times new roman;">
@@ -44,7 +43,7 @@
                             </div>
 
                         </div>
-                        <div id="pad2" style="display: none;" class="padcontent">
+                        <div id="price2" style="display: none;" class="priceTitle">
                             <div style="text-align: center; color: #055699; font-size: 18px; margin-top: 20px; margin-bottom: 5px; font-family: 'times new roman'; font-weight: bold;">
                                 BÁO GIÁ BANNER PHIÊN BẢN DESKTOP
                             </div>
@@ -54,7 +53,7 @@
                             </div>
                         </div>
 
-                        <div style="padding:15px;  text-align: justify; text-rendering: geometricPrecision; clear: both">
+                        <div class="p-5" style="text-align: justify; clear: both">
                             <p>
                                 <?php echo $price->content ?>
                             </p>
@@ -74,18 +73,18 @@
     $(document).ready(function () {
         if (window.location.hash) {
             var hash = window.location.hash.substring(1, 2);
-            if ($.isNumeric(hash)) showHeaderPrice(hash);
+            if ($.isNumeric(hash)) showTitlePrice(hash);
         } else {
-            showHeaderPrice(1);
+            showTitlePrice(1);
         }
     });
 
-    function showHeaderPrice(id) {
-        $(".padcontent").each(function () {
+    function showTitlePrice(id) {
+        $(".priceTitle").each(function () {
             $(this).css("display", "none");
         });
 
-        $("#pad" + id).css("display", "block");
+        $("#price" + id).css("display", "block");
     }
 
 </script>
