29064f065a21ff0d0f7d8f755cab4a5f2470deb6
thinhlv <thinhluuvan@admicro.vn>
2019-10-15 09:29:23 +0700

    update

--- application/views/site/header_view.php
+++ application/views/site/header_view.php
@@ -41,7 +41,6 @@
     <!--<script src="--><?php //echo public_url('js/jquery.min.js')?><!--"></script>-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
     <script src="<?php echo public_url('js/bootstrap.min.js') ?>"></script>
-    <script src="<?php echo base_url('public/js/page.js'); ?>"></script>
     <script src="<?php echo base_url('public/js/common.js'); ?>"></script>
     <script src="<?php echo base_url('public/js/lightslider.js'); ?>"></script>
     <script src="<?php echo base_url('public/js/bxslider/jquery.bxslider.min.js'); ?>"></script>
