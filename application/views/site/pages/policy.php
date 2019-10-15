47c09f416dae42c7cc8f5f7ba84b2b8185f08a1f
thinhlv <thinhlv02@wru.vn>
2019-10-15 21:46:58 +0700

    udpate at home

--- application/views/site/pages/policy.php
+++ application/views/site/pages/policy.php
@@ -4,20 +4,19 @@
             <div class="left-menu">
                 <div class="left-title">Chính sách và điều khoản</div>
                 <ul>
-                    <li class="<?php if ($active == 99) echo 'active' ?>"><a href="<?php echo base_url('dieu-khoan-su-dung.html') ?>">Điều khoản sử dụng</a></li>
-                    <li class="<?php if ($active == 100) echo 'active' ?>"><a href="<?php echo base_url('chinh-sach-bao-mat.html') ?>">Chính sách bảo mật</a></li>
+                    <li class="<?php if ($active == 99) echo 'active' ?>"><a
+                                href="<?php echo base_url('dieu-khoan-su-dung.html') ?>">Điều khoản sử dụng</a></li>
+                    <li class="<?php if ($active == 100) echo 'active' ?>"><a
+                                href="<?php echo base_url('chinh-sach-bao-mat.html') ?>">Chính sách bảo mật</a></li>
                 </ul>
             </div>
         </div>
         <div class="col-md-9 col-sm-8 detail-content">
-            <h2><?php echo $title ?></h2>
             <div class="card line-height-2">
                 <div class="card-body">
                     <?php echo $page_content ?>
                 </div>
             </div>
-
-
         </div>
     </div>
 </section>
