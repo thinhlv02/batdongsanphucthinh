62b3a57194e6c7ffe8af53d4d85cbd836fa701ef
thinhlv <thinhluuvan@admicro.vn>
2019-10-17 17:24:01 +0700

    update

---
+++ application/views/site/faq/front-end.php
@@ -0,0 +1,18 @@
+<section class="detail">
+    <div class="container">
+        <div class="col-sm-12 col-md-12">
+            <div class="left-menu">
+                <div class="left-title">Những câu hỏi thường gặp</div>
+
+
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<script type="text/javascript">
+
+
+</script>
