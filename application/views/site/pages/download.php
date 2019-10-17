15bf68ec0fe9a06488d79a2153a4711fcbd83899
thinhlv <thinhluuvan@admicro.vn>
2019-10-17 14:49:44 +0700

    update

--- application/views/site/pages/download.php
+++ application/views/site/pages/download.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <?php $this->load->view($this->_template_f . 'layout/head')?>
+    <?php $this->load->view($this->_template_f . 'header_view') ?>
 </head>
 <body>
-<?php $this->load->view($this->_template_f . 'layout/menu')?>
+<?php $this->load->view($this->_template_f . 'menu_view') ?>
 
 <div style="margin-top: 60px">
-    <?php $this->load->view($this->_template_f . 'home/download')?>
+    <?php $this->load->view($this->_template_f . 'home/download') ?>
 </div>
 
-<?php $this->load->view($this->_template_f . 'layout/footer')?>
+<?php $this->load->view($this->_template_f . 'footer_view') ?>
 </body>
 </html>
