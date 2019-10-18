7f90b841cc5aec1d759c89021bf48b9dfe4f3aaa
thinhlv <thinhluuvan@admicro.vn>
2019-10-18 15:06:43 +0700

    update

--- application/views/saola/menu_view.php
+++ application/views/saola/menu_view.php
@@ -21,7 +21,7 @@
         <li class="dropdown <?php echo !empty($li_8) ? 'menu-active' : ''?>" title="<?php echo $common_lang['lmenu_phonebook']; ?>">
             <a href="javascript:void(0)"> <?php echo $common_lang['lmenu_phonebook']; ?><i class="mdi mdi-menu-down"></i></a>
             <div class="dropdown-content">
-                <a href="javascript:void(0)"><?php echo $common_lang['lbl_menu_agency']; ?></a>
+                <a href="<?php echo base_url('dai-ly')?>"><?php echo $common_lang['lbl_menu_agency']; ?></a>
             </div>
         </li>
         <li class="<?php echo !empty($li_5) ? 'menu-active' : ''?>" title="<?php echo $common_lang['lmenu_contact']; ?>"><a href="<?php echo base_url('lien-he')?>"> <?php echo $common_lang['lmenu_contact']; ?></a></li>
