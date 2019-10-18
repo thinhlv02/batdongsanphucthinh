a077da61f2abdc699c8bdbc013a83036d1e769b9
thinhlv <thinhluuvan@admicro.vn>
2019-10-18 12:01:40 +0700

    update

--- application/language/vietnamese/common_lang.php
+++ application/language/vietnamese/common_lang.php
@@ -13,6 +13,7 @@ $lang['common_lang']['product'] = "Sản phẩm";
 $lang['common_lang']['recruit'] = 'Tuyển dụng';
 $lang['common_lang']['lmenu_contact'] = 'Liên hệ';
 $lang['common_lang']['lmenu_faq'] = 'Câu hỏi';
+$lang['common_lang']['lmenu_phonebook'] = 'Danh bạ';
 $lang['common_lang']['login'] = 'Đăng nhập';
 $lang['common_lang']['register'] = 'Đăng ký';
 $lang['common_lang']['oops'] = 'Oops!';
