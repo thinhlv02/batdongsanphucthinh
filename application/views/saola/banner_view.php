76b2a65e36de6e6b33b8f321564c8ea53e90b3fe
thinhlv <thinhluuvan@admicro.vn>
2019-10-21 11:52:02 +0700

    update

--- application/views/saola/banner_view.php
+++ application/views/saola/banner_view.php
@@ -21,11 +21,11 @@
 
 <div id='ads-left'>
     <div style='margin:0 0 5px 0; padding:0;width:160px;position:fixed; left:0; top:47px;'>
-        <a href='<?php echo base_url() ?>' target='_blank'><img border='0' height='600' src='<?php echo public_url('images/') ?>ads/ads-136-left.jpg' width='160'/></a>
+        <a href='http://batdongsanphucthinh.vn/rao-vat/can-ban-nha-chinh-chu-lien-he-chinh-chu-0937-535-448-136' target='_blank'><img border='0' height='600' src='<?php echo public_url('images/') ?>ads/ads-136-left.jpg' width='160'/></a>
     </div>
 </div>
 <div id='ads-right'>
     <div style='margin:0 0 5px 0; padding:0;width:160px;position:fixed; right:0; top:47px;'>
-        <a href='<?php echo base_url() ?>' target='_blank'><img border='0' height='600' src='<?php echo public_url('images/') ?>ads/ads-136-left.jpg' width='160'/></a>
+        <a href='http://batdongsanphucthinh.vn/rao-vat/can-ban-nha-chinh-chu-lien-he-chinh-chu-0937-535-448-136' target='_blank'><img border='0' height='600' src='<?php echo public_url('images/') ?>ads/ads-136-left.jpg' width='160'/></a>
     </div>
 </div>
