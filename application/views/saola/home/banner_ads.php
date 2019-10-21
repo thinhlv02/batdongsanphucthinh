fa7b45e2de8ddd34ea6979aa93691aafb9cdbd89
thinhlv <thinhluuvan@admicro.vn>
2019-10-21 14:41:51 +0700

    update

--- application/views/saola/home/banner_ads.php
+++ application/views/saola/home/banner_ads.php
@@ -1,6 +1,6 @@
 <div id='ads-left'>
     <div style='margin:0 0 5px 0; padding:0;width:160px;position:fixed; left:0; top:47px;'>
-        <a href='http://batdongsanphucthinh.vn/rao-vat/can-ban-nha-chinh-chu-lien-he-chinh-chu-0937-535-448-136' target='_blank'><img border='0' height='600' src='<?php echo public_url('images/') ?>ads/ads-136-left.jpg' width='160'/></a>
+        <a href='http://batdongsanphucthinh.vn/rao-vat/can-ban-nha-2-mat-tien-dep-chinh-chu-doi-dien-cang-phan-thiet-phu-quy-lien-he-0913883236-128' target='_blank'><img border='0' height='600' src='<?php echo public_url('images/') ?>ads/128left.jpg' width='160'/></a>
     </div>
 </div>
 <div id='ads-right'>
