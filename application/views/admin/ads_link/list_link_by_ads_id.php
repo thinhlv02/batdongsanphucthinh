d0ee14208b247d16b96e40efe3f3eb932b16dbc8
thinhlv <thinhluuvan@admicro.vn>
2019-10-21 10:27:03 +0700

    update

--- application/views/admin/ads_link/list_link_by_ads_id.php
+++ application/views/admin/ads_link/list_link_by_ads_id.php
@@ -1,7 +1,9 @@
-
+<?php
+$id_ads = $this->uri->segment(4);
+?>
 <div class="x_panel">
     <div class="x_title">
-        <h2>Danh sách bài đăng(<?php echo $count> 0 ? number_format($count) : $count ?>)</h2>
+        <h2>Quản lý mã tin rao : <span class="" style="color: red!important;"><?php echo $id_ads; ?></span></h2>
         <ul class="nav navbar-right panel_toolbox">
             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
             <li class="dropdown">
@@ -29,27 +31,26 @@
             </thead>
             <tbody>
             <?php
-            $id_ads = $this->uri->segment(4);
-            foreach ($ads as $row){ ?>
+            foreach ($ads as $row) { ?>
                 <tr title="" class="">
                     <td class="text-center">
                         <button class="btn btn-default btn-xs"><?php echo $row->id ?></button>
-                        <a class="btn btn-xs btn-primary btn-xs" href="<?php echo base_url('admin/ads/edit_link/'.$row->id.'/'.$id_ads)?>">Sửa</a>
-                            <a class="btn btn-xs btn-danger" onclick="confirmDel(<?php echo $row->id?>, <?php echo $id_ads; ?>)">Xóa</a>
+                        <a class="btn btn-xs btn-primary btn-xs" href="<?php echo base_url('admin/ads/edit_link/' . $row->id . '/' . $id_ads) ?>">Sửa</a>
+                        <a class="btn btn-xs btn-danger" onclick="confirmDel(<?php echo $row->id ?>, <?php echo $id_ads; ?>)">Xóa</a>
                     </td>
                     <td><?php echo date('d/m/Y', strtotime($row->created_at)); ?></td>
                     <td><?php echo $row->link_web ?></td>
                     <td><?php echo $row->link_facebook ?></td>
                 </tr>
 
-            <?php }?>
+            <?php } ?>
             </tbody>
         </table>
     </div>
 </div>
 <script>
     function confirmDel(id, id_ads) {
-        if(confirm('Bạn có chắc chắn muốn xóa?')){
+        if (confirm('Bạn có chắc chắn muốn xóa?')) {
             window.location.href = '<?php echo base_url('admin/ads/del_link/')?>' + id + '/' + id_ads;
         }
     }
