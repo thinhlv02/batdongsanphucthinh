76455930cccac8e2b2c94ca85ed196654128afdb
thinhlv <thinhlv02@wru.vn>
2019-10-21 22:33:29 +0700

    udpate at home

--- application/views/saola/ads/ads_detail_link.php
+++ application/views/saola/ads/ads_detail_link.php
@@ -11,7 +11,7 @@
                             <i class="fa fa-angle-right"></i> <?php echo $ads->title ?></strong>
                     </p>
 
-                    <div class="container12">
+                    <div class="">
 
                         <ul class="nav nav-tabs">
                             <li class="active"><a data-toggle="tab" href="#home">Danh sách bài đăng
@@ -36,7 +36,7 @@
                                                         <?php
                                                         $tags_link = explode('a href="', trim($v1->link_web));
                                                         ?>
-                                                        <h2>Ngày đẩy tin: <?php echo $v1->created_at; ?></h2>
+                                                        <h5>Ngày đẩy tin: <?php echo $v1->created_at; ?></h5>
                                                         <table class="table table-bordered">
                                                             <thead>
                                                             <tr>
@@ -74,7 +74,6 @@
 
                                 </div>
 
-
                             </div>
                             <div id="menu1" class="tab-pane fade">
 
@@ -92,7 +91,7 @@
                                                         <?php
                                                         $tags_link = explode('a href="', trim($v1->link_facebook));
                                                         ?>
-                                                        <h2>Ngày đẩy tin: <?php echo $v1->created_at; ?></h2>
+                                                        <h5>Ngày đẩy tin: <?php echo $v1->created_at; ?></h5>
                                                         <table class="table table-bordered">
                                                             <thead>
                                                             <tr>
@@ -147,58 +146,10 @@
 </script>
 
 <style>
-    /*old slider*/
-    ul {
-        list-style: none outside none;
-        padding-left: 0;
-        margin-bottom: 0;
-    }
-
-    li {
-        /*display: block;*/
-        display: list-item;
-        /*float: left;*/
-        margin-right: 6px;
-        cursor: pointer;
-    }
-
-    img {
-        display: block;
-        /*height: 703px;*/
-        height: auto;
-        /*max-height: 920px;*/
-        max-width: 100%;
-    }
-
-    /*old slider*/
-
-    .brief_land_box .ibrief {
-        line-height: 24px;
-    }
-
-    .land_page .title_land_box {
-        background: #f8f8f8;
-        color: #e40b00;
-        font-size: 17px;
-        font-weight: 600;
-        padding: 10px;
-        margin: 15px 0 5px 0;
-    }
-
     .land_page .pack_land_box .pland {
         border-bottom: 1px solid #e0e0e0;
         border-left: 1px solid #e0e0e0;
         color: #333;
-    }
-
-    .land_page .pack_land_box .pland .title_detail_pland {
-        font-weight: 600;
-        font-size: 18px;
-        padding: 10px 10px 0 10px;
-    }
-
-    .land_page .pack_land_box .pland .body_detail_pland {
-        padding: 5px 10px 10px 10px;
     }
 
     .land_page .pack_land_box {
@@ -212,10 +163,6 @@
         display: inline-block;
         padding-left: 10px;
         width: 110px;
-    }
-
-    .land_page .poster_land_box .pic_poster_land_box {
-        padding-right: 30px;
     }
 
     .land_page .poster_land_box .detail_poster_land_box p:first-child {
@@ -242,11 +189,6 @@
         font-size: 16px;
     }
 
-    .land_page .title1_land_box {
-        border-bottom: 1px solid #ddd;
-        margin: 10px 0 20px 0;
-    }
-
     p.title_box1 strong {
         display: inline-block;
         font-size: 16px;
@@ -256,15 +198,8 @@
         border-left: 5px solid #4dbb6d;
     }
 
-    .body_top2_box {
-        padding: 15px;
-        border: 1px solid #ddd;
-        border-top: 0;
-    }
-
-    .land_page .land_box h1, .land_page .land_box h2 {
+    .land_page .land_box h1, .land_page .land_box h2, .land_box h5 {
         color: #e40b00;
-        font-size: 20px;
         text-transform: uppercase;
         font-weight: 600;
         margin-bottom: 10px;
@@ -279,16 +214,6 @@
         border: 1px solid #ddd;
         padding: 4px;
         margin-top: 5px;
-    }
-
-    p.title_box1 {
-        background: #efefef;
-        border-bottom: 1px solid #ddd;
-    }
-
-    .land_page .price_ibrief {
-        font-size: 20px;
-        color: #e40b00;
     }
 
     /*//css tab active*/
