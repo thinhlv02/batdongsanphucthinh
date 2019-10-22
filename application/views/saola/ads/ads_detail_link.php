3953a7cc03edc144387bba5752a7208eb2f1611b
thinhlv <thinhluuvan@admicro.vn>
2019-10-22 09:03:15 +0700

    update

--- application/views/saola/ads/ads_detail_link.php
+++ application/views/saola/ads/ads_detail_link.php
@@ -29,18 +29,18 @@
                                     <div style="clear: both;"></div>
 
                                     <?php foreach ($ads_end as $k1 => $v1) { ?>
-                                        <div class="pack_land_box" style="border: 0;">
+                                        <div class="pack_land_box">
                                             <div class="row">
-                                                <div class="col-xs-12 pland" style="border: 0;">
+                                                <div class="col-xs-12 pland">
                                                     <div class="ads_link">
                                                         <?php
                                                         $tags_link = explode('a href="', trim($v1->link_web));
                                                         ?>
-                                                        <h5>Ngày đẩy tin: <?php echo $v1->created_at; ?></h5>
+                                                        <h5 class="text-uppercase">Ngày đẩy tin: <?php echo $v1->created_at; ?></h5>
                                                         <table class="table table-bordered">
                                                             <thead>
                                                             <tr>
-                                                                <th>STT</th>
+                                                                <th>#</th>
                                                                 <th>Link website</th>
                                                             </tr>
                                                             </thead>
@@ -91,11 +91,11 @@
                                                         <?php
                                                         $tags_link = explode('a href="', trim($v1->link_facebook));
                                                         ?>
-                                                        <h5>Ngày đẩy tin: <?php echo $v1->created_at; ?></h5>
+                                                        <h5 class="text-uppercase">Ngày đẩy tin: <?php echo $v1->created_at; ?></h5>
                                                         <table class="table table-bordered">
                                                             <thead>
                                                             <tr>
-                                                                <th>STT</th>
+                                                                <th>#</th>
                                                                 <th>Link Facebook</th>
                                                             </tr>
                                                             </thead>
@@ -146,74 +146,14 @@
 </script>
 
 <style>
-    .land_page .pack_land_box .pland {
-        border-bottom: 1px solid #e0e0e0;
-        border-left: 1px solid #e0e0e0;
-        color: #333;
-    }
-
-    .land_page .pack_land_box {
-        border-top: 1px solid #e0e0e0;
-        border-right: 1px solid #e0e0e0;
-    }
-
-    .land_page .pack_land_box .pland > strong {
-        line-height: 32px;
-        font-weight: 600;
-        display: inline-block;
-        padding-left: 10px;
-        width: 110px;
-    }
-
-    .land_page .poster_land_box .detail_poster_land_box p:first-child {
-        border-top: 0;
-    }
-
-    .land_page .poster_land_box .detail_poster_land_box p {
-        padding: 5px 0;
-        border-top: 1px dotted #ddd;
-    }
-
-    .land_page .land_box {
-        border: 1px solid #ddd;
-        padding: 15px;
-    }
-
-    .land_page .title1_land_box strong {
-        background: #fff;
-        display: inline-block;
-        position: relative;
-        margin-bottom: -10px;
-        font-weight: 600;
-        line-height: 20px;
-        font-size: 16px;
-    }
-
-    p.title_box1 strong {
-        display: inline-block;
-        font-size: 16px;
-        line-height: 50px;
-        font-weight: 600;
-        padding-left: 10px;
-        border-left: 5px solid #4dbb6d;
-    }
-
     .land_page .land_box h1, .land_page .land_box h2, .land_box h5 {
         color: #e40b00;
-        text-transform: uppercase;
         font-weight: 600;
-        margin-bottom: 10px;
     }
 
     .land_page .land_box {
         border: 1px solid #ddd;
         padding: 15px;
-    }
-
-    .land_page .poster_land_box .pic_poster_land_box p {
-        border: 1px solid #ddd;
-        padding: 4px;
-        margin-top: 5px;
     }
 
     /*//css tab active*/
