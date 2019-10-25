8a966b188d27d0e8293b056d23bd707b63dedcf4
thinhlv <thinhlv02@wru.vn>
2019-10-25 22:53:15 +0700

    udpate at home

--- application/views/saola/footer_view.php
+++ application/views/saola/footer_view.php
@@ -84,7 +84,7 @@
                             <!--                            </a>-->
 
                             <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fbatdongsanphucthinhvn-1535407536595838%2F%3Fmodal%3Dadmin_todo_tour&tabs&width=340&height=214&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true&appId=166934463979079"
-                                    width="340" height="214" style="border:none;overflow:hidden" scrolling="no"
+                                    width="340" height="214" style="border:none;overflow:hidden;width: 100%;" scrolling="no"
                                     frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 
                         </div>
