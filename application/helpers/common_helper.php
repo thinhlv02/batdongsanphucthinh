e2f7c9a5d5a758730264b9f84fafd7188493c52e
thinhlv <thinhluuvan@admicro.vn>
2019-10-25 14:02:53 +0700

    update

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -58,8 +58,8 @@ function config_pagination($per_page = 10, $segment = 1, $total = 0, $base_url =
     $config['base_url'] = $base_url;
     $config['total_rows'] = $total;
     $config['per_page'] = $per_page;
-    $config['prev_link'] = '&lt;';
-    $config['next_link'] = '&gt;';
+    $config['prev_link'] = '&laquo;';
+    $config['next_link'] = '&raquo;';
     $config['last_link'] = 'Cuối';
     $config['first_link'] = 'Đầu';
     $config['use_page_numbers'] = TRUE;
