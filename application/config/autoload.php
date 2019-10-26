db3bf57f4ecb2ff7fca02b671d8f8407015c544c
thinhlv <thinhlv02@wru.vn>
2019-10-26 23:04:54 +0700

    udpate at home

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','file','common','thumb');
+$autoload['helper'] = array('url','file','common','thumb','menu');
 
 /*
 | -------------------------------------------------------------------
