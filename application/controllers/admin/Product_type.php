d74dff2ba6c6530d3c6630c76ff2d5d7d2983be8
thinhlv <thinhlv02@wru.vn>
2019-10-29 20:00:40 +0700

    udpate at home

--- application/controllers/admin/Product_type.php
+++ application/controllers/admin/Product_type.php
@@ -5,12 +5,12 @@ Class Product_type extends MY_Controller
     function __construct()
     {
         parent::__construct();
-        $this->load->model('product_type_model');
+        $this->load->model('category_model');
     }
 
     function index()
     {
-        $questions = $this->product_type_model->get_list(array('where' => array('type' => 1), 'order' => array('id', 'asc')));
+        $questions = $this->category_model->get_list(array('where' => array('type' => 1), 'order' => array('id', 'asc')));
 //        pre($questions);
         $this->data['tab'] = 1;
         $this->data['ads_type'] = $this->_product_type;
@@ -23,7 +23,7 @@ Class Product_type extends MY_Controller
     function get_list_by_type()
     {
         $type = intval($_POST['type']);
-        $questions = $this->product_type_model->get_list(array('where' => array('type' => $type)));
+        $questions = $this->category_model->get_list(array('where' => array('type' => $type)));
         $this->data['questions'] = $questions;
         $this->load->view('admin/product_type/table_question', $this->data);
     }
@@ -39,7 +39,7 @@ Class Product_type extends MY_Controller
                 'content' => '',
                 'type' => intval($this->input->post('slType')),
             );
-            if ($this->product_type_model->create($data_submit)) {
+            if ($this->category_model->create($data_submit)) {
                 $this->session->set_flashdata('message', 'Thêm thành công!');
                 redirect(base_url('admin/product_type'));
             } else {
@@ -58,12 +58,12 @@ Class Product_type extends MY_Controller
         $message = $this->session->flashdata('message');
         $this->data['message'] = $message;
         $id = $this->uri->segment(4);
-        $question = $this->product_type_model->get_info($id);
+        $question = $this->category_model->get_info($id);
         if (!$question) {
             redirect(admin_url('ads_type'));
         }
 
-        $questions = $this->product_type_model->get_list(array('where' => array('id' => $question->id)));
+        $questions = $this->category_model->get_list(array('where' => array('id' => $question->id)));
         $this->data['questions'] = $questions;
         $this->data['view'] = 'admin/product_type/edit_level_1';
 
@@ -73,7 +73,7 @@ Class Product_type extends MY_Controller
                 'name' => $this->input->post('txtName'),
                 'routes' => $this->input->post('txtRoutes'),
             );
-            if ($this->product_type_model->update($id, $data_submit)) {
+            if ($this->category_model->update($id, $data_submit)) {
                 $this->session->set_flashdata('message', 'Cập nhật thành công!');
                 redirect(base_url('admin/product_type/edit/' . $id));
             } else {
@@ -92,11 +92,11 @@ Class Product_type extends MY_Controller
     {
 //        pre(1);
         $id = $this->uri->segment(4);
-        $question = $this->product_type_model->get_info($id);
+        $question = $this->category_model->get_info($id);
         if (!$question) {
             redirect(admin_url('ads_type'));
         }
-        $this->product_type_model->delete($id);
+        $this->category_model->delete($id);
         $this->session->set_flashdata('message', 'Xoá thành công!');
         if ($question->level == 1) {
             redirect(admin_url('ads_type'));
--- application/models/Product_type_model.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Class Product_type_model extends MY_Model
-{
-    var $table = 'product_type';
-}
