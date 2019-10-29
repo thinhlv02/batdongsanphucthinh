159ebad5744dc326a01b51c1fbc7887a15c2616e
thinhlv <thinhluuvan@admicro.vn>
2019-10-29 14:56:13 +0700

    update

--- application/views/admin/product/edit.php
+++ application/views/admin/product/edit.php
@@ -1,13 +1,16 @@
-<!--<script language="javascript" src="--><?php //echo base_url('public')?><!--/ckeditor/ckeditor.js" type="text/javascript"></script>-->
+<!--<script language="javascript" src="--><?php //echo base_url('public') ?><!--/ckeditor/ckeditor.js" type="text/javascript"></script>-->
 <script type="text/javascript" src="<?php echo base_url(); ?>public/scripts/ckeditor/ckeditor.js"></script>
 
 <div class="x_panel">
     <div class="x_title">
-        <h2>Sửa thông tin</h2>
+        <h2>Chỉnh sửa rao vặt: <span class="text-danger" style="color: red !important;"><?php echo $ads->code ?></span>
+
+       Mã màu chữ: <span class="text-primary "  style="color: #ff9900">#eb5c66; font-size: 36pt, font-family: tahoma, stroke: 3pt, lineheight: 48pt</span></h2>
         <ul class="nav navbar-right panel_toolbox">
             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
+                            class="fa fa-wrench"></i></a>
                 <ul class="dropdown-menu" role="menu">
                     <li><a href="#">Settings 1</a>
                     </li>
@@ -23,40 +26,308 @@
 
 
         <div class="row">
-            <form id="formAddCatalog" data-parsley-validate class="form-horizontal form-label-left" method="post" enctype="multipart/form-data">
+            <form id="formAddCatalog" data-parsley-validate class="form-horizontal form-label-left" method="post"
+                  enctype="multipart/form-data">
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tiêu đề <span class="required">*</span></label>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <input type="text" id="txtName" name="txtName" value="<?php echo $product->name?>" required="required" class="form-control col-md-7 col-xs-12">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tiêu đề
+                        <span class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <input type="text" id="txtName" name="txtName" value="<?php echo $ads->title ?>"
+                                class="form-control col-md-7 col-xs-12" placeholder="Tên bài viết">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Mô tả ngắn <span class="required">*</span></label>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <textarea name="txtIntro" class="form-control" style="height: 80px"><?php echo $product->intro?></textarea>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Giới thiệu ngắn
+                        <span class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <textarea name="txtIntro" rows="4"
+                                  class="form-control"><?php echo str_replace('<br />', '&#13;', $ads->intro) ?></textarea>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Ảnh minh họa <span class="required">*</span></label>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <img src="<?php echo public_url('images/products/'.$product->img)?>" width="200px">
-                        <input type="file" class="form-control" name="img_product" id="img_news" style="margin-top: 10px">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Ảnh minh họa<span
+                                class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <input type="file" class="form-control" name="img_news" id="img_news">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Ảnh Slide <span class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <input type="file" class="form-control" name="files[]" multiple>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Nội dung
+                        <span class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <textarea name="txtContent" class="form-control"
+                                  style="height: 120px"><?php echo $ads->content ?></textarea>
+                        <script type="text/javascript">CKEDITOR.replace('txtContent', {height: '500px'}); </script>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Nội dung <span class="required">*</span></label>
-                    <div class="col-md-8 col-sm-8 col-xs-12">
-                        <textarea name="txtContent" class="form-control" style="height: 120px"><?php echo $product->content?></textarea>
-                        <script type="text/javascript">CKEDITOR.replace('txtContent',{height: '300px'}); </script>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Link site
+                        <span class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <textarea name="txtLinkSite" class="form-control"
+                                  style="height: 120px"><?php echo $ads->link_web ?></textarea>
+                        <script type="text/javascript">CKEDITOR.replace('txtLinkSite', {height: '500px'}); </script>
                     </div>
                 </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Giá tiền <span
+                                class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <input type="text" name="price" value="<?php echo $ads->price ?>"
+                               class="form-control col-md-7 col-xs-12" placeholder="ví dụ : 1,2 tỷ / nền">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Diện tích MB<span
+                                class="required">*</span></label>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <input type="text" name="acreage" value="<?php echo $ads->acreage ?>"
+                                class="form-control col-md-12 col-xs-12" placeholder="ví dụ: 90">
+                    </div>
+
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Diện tích SD<span
+                                class="required">*</span></label>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <input type="text" name="useacreage" value="<?php echo $ads->useacreage ?>"
+                                class="form-control col-md-12 col-xs-12" placeholder="ví dụ: 90">
+                    </div>
+
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Mặt tiền<span
+                                class="required">*</span></label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <input type="text" name="width"  value="<?php echo $ads->width ?>"
+                               class="form-control col-md-7 col-xs-12" placeholder="ví dụ: 10">
+                    </div>
+
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Đường vào<span
+                                class="required">*</span></label>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <input type="text" name="landwidth" value="<?php echo $ads->landwidth ?>"
+                                class="form-control col-md-12 col-xs-12" placeholder="ví dụ: 20">
+                    </div>
+
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Khu vực<span
+                                class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <input type="text" name="area"  value="<?php echo $ads->area ?>"
+                               class="form-control col-md-7 col-xs-12" placeholder="Địa chỉ tin rao vặt">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tên liên lạc<span
+                                class="required">*</span></label>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <input type="text" name="txtContactName" value="<?php echo $ads->contact_name; ?>"
+                               class="form-control col-md-5 col-xs-12" placeholder="tên liên hệ">
+                    </div>
+
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">SĐT liên hệ<span
+                                class="required">*</span></label>
+                    <div class="col-md-4 col-sm-4 col-xs-12">
+                        <input type="text" name="phone"  value="<?php echo $ads->phone ?>"
+                               class="form-control col-md-5 col-xs-12" placeholder="sđt liên hệ">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tỉnh / TP<span
+                                class="required">*</span></label>
+                    <div class="col-md-2 col-sm-2 col-xs-12">
+                        <select class="select2_group form-control" name="province" onchange="get_district(this)">
+                            <option value="0">-- Chọn Tỉnh/TP --</option>
+
+                            <?php foreach ($lstProvince as $key => $value) { ?>
+                                <option value="<?= $value->id ?>" <?php if (isset($province_id) && $province_id == $value->id) echo 'selected' ?>>
+                                    <?php echo $value->_name ?>
+                                </option>
+                            <?php } ?>
+
+                        </select>
+                    </div>
+
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Quận / Huyện<span
+                                class="required">*</span></label>
+                    <div class="col-md-2 col-sm-2 col-xs-12" id="divDistrict">
+                            <select class="select2_group form-control" name="district" id="selectDistrict" onchange="get_ward(this)">
+
+                            <option value="0">Chọn Quận/Huyện</option>
+                            <?php if (isset($district_arr) && !empty($district_arr)) {
+                                foreach ($district_arr as $k => $val) { ?>
+                                    <option value="<?php echo $val->id ?>" <?php if (isset($district_id) && $district_id == $val->id) echo 'selected' ?>>
+                                        <?php echo $val->_name ?>
+                                    </option>
+                                <?php }
+                            } ?>
+                        </select>
+
+                    </div>
+
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12 " for="first-name">Xã / Phường<span
+                                class="required">*</span></label>
+
+                    <div class="col-md-2 col-sm-2 col-xs-12" id="divWard">
+                        <select class="select2_group form-control" name="ward" id="selectWard" onchange="get_street(this)">
+
+                        <option value="0">Chọn Xã/Phường</option>
+                            <?php if (isset($ward_arr) && !empty($ward_arr)) {
+                                foreach ($ward_arr as $k => $val) {
+                                    $first = explode('|', $val['id']);
+//                                    echo $first[0]. '<br/>';
+                                    ?>
+                                    <option value="<?php echo $val['id'] ?>" <?php if (isset($ward_id) && $ward_id == $first[0]) echo 'selected' ?>>
+                                        <?php echo $val['_name'] ?>
+                                    </option>
+                                <?php }
+                            } ?>
+                        </select>
+
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12 " for="first-name">Đường phố<span
+                                class="required">*</span></label>
+
+                    <div class="col-md-2 col-sm-2 col-xs-12" id="divStreet">
+                        <select class="select2_group form-control" name="street">
+                            <option value="0">-- Không có --</option>
+
+                            <?php
+                            if (isset($street_arr) && !empty($street_arr)) {
+                                foreach ($street_arr as $k => $val) { ?>
+                                    <option value="<?php echo $val->id ?>" <?php if (isset($street_id) && $street_id == $val->id) echo 'selected' ?>>
+                                        <?php echo $val->_name ?>
+                                    </option>
+                                <?php }
+                            } ?>
+
+                        </select>
+                    </div>
+
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Lượt xem<span class="required">*</span></label>
+                    <div class="col-md-5 col-sm-5 col-xs-12">
+                        <input type="number" name="view" value="<?php echo $ads->view ?>"  class="form-control col-md-7 col-xs-12" placeholder="nhập lượt xem">
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Linh bài viết<span class="required">*</span></label>
+                    <div class="col-md-10 col-sm-10 col-xs-12">
+                        <input type="text" name="link" value="<?php echo $ads->link ?>" class="form-control col-md-7 col-xs-12" placeholder="nhập link bài viết">
+                    </div>
+                </div>
+
                 <div class="form-group" style="margin-top: 30px">
                     <div class="col-md-4 col-sm-4 col-xs-12 col-md-offset-2" style="width: 70px">
-                        <input type="submit" id="btnEditProduct" name="btnEditProduct" required="required" class="btn btn-primary" value="Cập nhật">
+                        <input type="submit" id="btnAddProduct" name="btnEdit"
+                               class="btn btn-primary" value="Cập nhật">
                     </div>
                 </div>
             </form>
         </div>
     </div>
 </div>
+
+
+<script>
+    function get_district(sel) {
+        var id = sel.value;
+        console.log(id);
+        if (id == 0) {
+            $('#selectDistrict').empty();
+            $('#selectWard').empty();
+            $('#selectStreet').empty();
+        } else {
+            // ajax
+            var params = {
+                'id': id
+            };
+
+            // console.log(params);
+            var _onSuccess = function (data) {
+                // console.log(data);
+                if (data == 'NOT_LOGIN') {
+                    window.location.reload(true);
+                } else if (data === 'false') {
+
+                } else {
+                    // console.log(data);
+                    $("#divDistrict").html(data);
+                }
+            };//
+
+            getAjax('<?php echo admin_url('ads/ajax_get_list_district'); ?>', params, '', 'GET', '', false, _onSuccess);
+        }
+    }
+
+    function get_ward(sel) {
+        var id = sel.value;
+        console.log(id);
+        if (id == 0) {
+            $('#selectWard').empty();
+            $('#selectStreet').empty();
+        } else {
+            var params = {
+                'id': id
+            };
+
+            var _onSuccess = function (data) {
+                // console.log(data);
+                if (data == 'NOT_LOGIN') {
+
+                } else if (data === 'false') {
+
+                } else {
+                    $("#divWard").html(data);
+                }
+            };
+
+            getAjax('<?php echo admin_url('ads/ajax_get_list_ward'); ?>', params, '', 'GET', '', false, _onSuccess);
+        }
+    }
+
+    function get_street(sel) {
+        var id = sel.value;
+        console.log(id);
+        if (id == 0) {
+            $('#selectStreet').empty();
+        } else {
+            // ajax
+            var params = {
+                'id': id
+            };
+
+            // console.log(params);
+            var _onSuccess = function (data) {
+                // console.log(data);
+                if (data == 'NOT_LOGIN') {
+
+                } else if (data === 'false') {
+
+                } else {
+                    // console.log(data);
+                    $("#divStreet").html(data);
+                }
+            };
+
+            getAjax('<?php echo admin_url('ads/ajax_get_list_street'); ?>', params, '', 'GET', '', false, _onSuccess);
+        }
+    }
+
+</script>
--- application/views/admin/product/index.php
+++ application/views/admin/product/index.php
@@ -1,38 +1,34 @@
-
 <div class="page-title" style="height: auto">
     <div class="title_left">
-        <h3>Dịch vụ</h3>
+        <h3>Quản lý bài đăng nhà đất</h3>
     </div>
     <div class="clearfix"></div>
     <div class="row">
         <div>
             <ul id="myTab" class="nav nav-tabs bar_tabs">
-                <li class="<?php echo $tab == 1 ? 'active' : ''?>">
-                    <a href="<?php echo admin_url('product')?>" id="home-tab">Danh sách</a>
+                <li class="<?php echo $tab == 1 ? 'active' : '' ?>">
+                    <a href="<?php echo admin_url('ads') ?>" id="home-tab">Danh sách</a>
                 </li>
-                <li class="<?php echo $tab == 2 ? 'active' : ''?>">
-                    <a href="<?php echo admin_url('product/add')?>" id="profile-tab2">Thêm mới</a>
+                <li class="<?php echo $tab == 2 ? 'active' : '' ?>">
+                    <a href="<?php echo admin_url('ads/add') ?>" id="profile-tab2">Thêm mới</a>
                 </li>
-                <?php if($tab == 3){ ?>
-                    <li class="<?php echo $tab == 3 ? 'active' : ''?>">
-                        <a href="#" id="profile-tab2">Cập nhật</a>
-                    </li>
-                <?php }?>
             </ul>
         </div>
     </div>
 </div>
 
-<?php if (isset($message)) {$this->load->view('admin/message', $this->data);} ?>
+<?php if (isset($message)) {
+    $this->load->view('admin/message', $this->data);
+} ?>
 
-<?php $this->load->view($view)?>
-
+<?php $this->load->view($view) ?>
 
 <style type="text/css">
-    td{
+    td {
         vertical-align: middle !important;
     }
-    .action a{
+
+    .action a {
         font-size: 22px;
         display: block;
         cursor: pointer;
