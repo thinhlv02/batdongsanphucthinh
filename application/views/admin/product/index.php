2521ce9e61b0811b0d245a32e79a88db2ed4f331
thinhlv <thinhluuvan@admicro.vn>
2019-10-29 16:59:06 +0700

    update

--- application/views/admin/product/index.php
+++ application/views/admin/product/index.php
@@ -7,10 +7,10 @@
         <div>
             <ul id="myTab" class="nav nav-tabs bar_tabs">
                 <li class="<?php echo $tab == 1 ? 'active' : '' ?>">
-                    <a href="<?php echo admin_url('ads') ?>" id="home-tab">Danh sách</a>
+                    <a href="<?php echo admin_url('product') ?>" id="home-tab">Danh sách</a>
                 </li>
                 <li class="<?php echo $tab == 2 ? 'active' : '' ?>">
-                    <a href="<?php echo admin_url('ads/add') ?>" id="profile-tab2">Thêm mới</a>
+                    <a href="<?php echo admin_url('product/add') ?>" id="profile-tab2">Thêm mới</a>
                 </li>
             </ul>
         </div>
