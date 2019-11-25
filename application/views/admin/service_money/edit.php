8a70c9e6ff74bd42adba7831373e28abf39cf0b1
thinhlv <thinhluuvan@admicro.vn>
2019-11-25 09:35:21 +0700

    update

--- application/views/admin/service_money/edit.php
+++ application/views/admin/service_money/edit.php
@@ -7,8 +7,7 @@
         <ul class="nav navbar-right panel_toolbox">
             <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
             <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i
-                            class="fa fa-wrench"></i></a>
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
                 <ul class="dropdown-menu" role="menu">
                     <li><a href="#">Settings 1</a>
                     </li>
@@ -23,22 +22,17 @@
     <div class="x_content">
 
         <div class="row">
-            <form id="formAddCatalog" data-parsley-validate class="form-horizontal form-label-left" method="post"
-                  enctype="multipart/form-data">
+            <form id="formAddCatalog" data-parsley-validate class="form-horizontal form-label-left" method="post" enctype="multipart/form-data">
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tiêu đề
-                        <span class="required">*</span></label>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Tiêu đề<span class="required">*</span></label>
                     <div class="col-md-8 col-sm-8 col-xs-12">
-                        <input type="text" id="txtName" name="txtName" readonly value="<?php echo $ads->title ?>"
-                               required="required" class="form-control col-md-7 col-xs-12" placeholder="Tên bài viết">
+                        <input type="text" id="txtName" name="txtName" readonly value="<?php echo $ads->title ?>" required="required" class="form-control col-md-7 col-xs-12" placeholder="Tên bài viết">
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Giới thiệu ngắn
-                        <span class="required">*</span></label>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Giới thiệu ngắn<span class="required">*</span></label>
                     <div class="col-md-8 col-sm-8 col-xs-12">
-                        <textarea name="txtIntro" rows="3" readonly
-                                  class="form-control"><?php echo str_replace('<br />', '&#13;', $ads->intro) ?></textarea>
+                        <textarea name="txtIntro" rows="3" readonly class="form-control"><?php echo str_replace('<br />', '&#13;', $ads->intro) ?></textarea>
                     </div>
                 </div>
 
@@ -46,39 +40,32 @@
                     <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">service_money
                         <span class="required">*</span></label>
                     <div class="col-md-8 col-sm-8 col-xs-12">
-                        <input type="text" name="service_money" id="service_money" value="<?php echo $ads->service_money ?>"
-                               required="required" class="form-control col-md-7 col-xs-12" placeholder="service_money">
+                        <input type="text" name="service_money" id="service_money" value="<?php echo $ads->service_money ?>" required="required" class="form-control col-md-7 col-xs-12" placeholder="service_money">
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">make_money_by
-                        <span class="required">*</span></label>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">make_money_by<span class="required">*</span></label>
                     <div class="col-md-8 col-sm-8 col-xs-12">
-                       <select class="form-control" name="make_money_by">
-                           <option value="0">Không</option>
-                           <?php foreach ($emps as $k => $val) {?>
-
-                            <option value="<?php echo $val->id ?>" <?php echo $val->id == $ads->make_money_by ? 'selected="selected"' : ''; ?> ><?php echo $val->name ?></option>
-                           <?php } ?>
-                       </select>
+                        <select class="form-control" name="make_money_by">
+                            <option value="0">Không</option>
+                            <?php foreach ($emps as $k => $val) { ?>
+                                <option value="<?php echo $val->id ?>" <?php echo $val->id == $ads->make_money_by ? 'selected="selected"' : ''; ?> ><?php echo $val->name ?></option>
+                            <?php } ?>
+                        </select>
                     </div>
                 </div>
 
                 <div class="form-group">
-                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">TG nạp<span
-                                class="required"></span></label>
+                    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">TG nạp<span class="required"></span></label>
                     <div class="col-md-2 col-sm-2 col-xs-12">
-                        <input type="text" id="txtFrom" name="date" required
-                               value="<?php echo $pay_time ?>"
-                               class="form-control col-md-7 col-xs-12"/>
+                        <input type="text" id="txtFrom" name="date" required value="<?php echo $pay_time ?>" class="form-control col-md-7 col-xs-12"/>
                     </div>
                 </div>
 
                 <div class="form-group" style="margin-top: 30px">
                     <div class="col-md-4 col-sm-4 col-xs-12 col-md-offset-2" style="width: 70px">
-                        <input type="submit" id="btnAddProduct" name="btnEdit" required="required"
-                               class="btn btn-primary" value="Cập nhật">
+                        <input type="submit" id="btnAddProduct" name="btnEdit" required="required" class="btn btn-warning" value="Cập nhật">
                     </div>
                 </div>
             </form>
@@ -87,8 +74,13 @@
 </div>
 
 <script>
-    $(document).ready(function()
-    {
-        $('#service_money').setMask({mask : '999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999', type : 'reverse', defaultValue: '0',textAlign:'false', textAlign: false});
+    $(document).ready(function () {
+        $('#service_money').setMask({
+            mask: '999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999,999',
+            type: 'reverse',
+            defaultValue: '0',
+            textAlign: 'false',
+            textAlign: false
+        });
     });
 </script>
