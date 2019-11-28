0bd34691d1f2dd76669bdc9205e2092de9f4765f
thinhlv <thinhluuvan@admicro.vn>
2019-11-28 09:37:27 +0700

    update

--- application/controllers/admin/Login.php
+++ application/controllers/admin/Login.php
@@ -1,20 +1,22 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-Class Login extends MY_Controller {
-	function __construct() {
-		parent::__construct();
+Class Login extends MY_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
 
-	}
+    }
 
-	function index()
+    function index()
     {
         $this->load->library('form_validation');
         $this->load->helper('form');
-        if($this->input->post())
+        if ($this->input->post())
         {
-            $this->form_validation->set_rules('login' ,'login', 'callback_check_login');
-            if($this->form_validation->run())
+            $this->form_validation->set_rules('login', 'login', 'callback_check_login');
+            if ($this->form_validation->run())
             {
                 $this->session->set_userdata('login', true);
 
@@ -35,8 +37,8 @@ Class Login extends MY_Controller {
         $password = md5($password);
 
         $this->load->model('admin_model');
-        $where = array('username' => $username , 'password' => $password);
-        if($this->admin_model->check_exists($where))
+        $where = array('username' => $username, 'password' => $password);
+        if ($this->admin_model->check_exists($where))
         {
             $this->load->model('admin_model');
             $input = array();
