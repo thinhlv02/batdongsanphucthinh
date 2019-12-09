959699035789db10f9f9921add53c085ae3af3b8
thinhlv <thinhluuvan@admicro.vn>
2019-12-09 16:53:16 +0700

    update

--- application/controllers/admin/Agency.php
+++ application/controllers/admin/Agency.php
@@ -1,8 +1,10 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-Class Agency extends MY_Controller {
-    function __construct() {
+Class Agency extends MY_Controller
+{
+    function __construct()
+    {
         parent::__construct();
         $this->load->model('agency_model');
     }
@@ -20,20 +22,24 @@ Class Agency extends MY_Controller {
         $this->load->view('admin/layout', $this->data);
     }
 
-    function add(){
+    function add()
+    {
         $message = $this->session->flashdata('message');
         $this->data['message'] = $message;
-        if($this->input->post('btnAdd')){
+        if ($this->input->post('btnAdd'))
+        {
             $data = array(
                 'name' => $this->input->post('txtName'),
                 'phone' => $this->input->post('txtPhone'),
                 'address' => $this->input->post('txtAddress'),
             );
-            if($this->agency_model->create($data)){
+            if ($this->agency_model->create($data))
+            {
                 $this->session->set_flashdata('message', 'Thêm thành công');
                 redirect(base_url('admin/agency'));
             }
-            else{
+            else
+            {
                 $this->session->set_flashdata('message', 'Lỗi thao tác cơ sở dữ liệu');
             }
         }
@@ -43,26 +49,31 @@ Class Agency extends MY_Controller {
         $this->load->view('admin/layout', $this->data);
     }
 
-    function edit(){
+    function edit()
+    {
         $message = $this->session->flashdata('message');
         $this->data['message'] = $message;
         $id = $this->uri->segment(4);
         $agency = $this->agency_model->get_info($id);
-        if(!$agency){
+        if (!$agency)
+        {
             redirect(base_url('admin/agency'));
         }
 
-        if($this->input->post('btnEdit')){
+        if ($this->input->post('btnEdit'))
+        {
             $data = array(
                 'name' => $this->input->post('txtName'),
                 'phone' => $this->input->post('txtPhone'),
                 'address' => $this->input->post('txtAddress'),
             );
-            if($this->agency_model->update($id, $data)){
+            if ($this->agency_model->update($id, $data))
+            {
                 $this->session->set_flashdata('message', 'Cập nhật thành công');
                 redirect(base_url('admin/agency'));
             }
-            else{
+            else
+            {
                 $this->session->set_flashdata('message', 'Lỗi thao tác cơ sở dữ liệu');
             }
         }
@@ -73,28 +84,34 @@ Class Agency extends MY_Controller {
         $this->load->view('admin/layout', $this->data);
     }
 
-    function del(){
+    function del()
+    {
         $id = $this->uri->segment(4);
         $agency = $this->agency_model->get_info($id);
-        if($agency){
+        if ($agency)
+        {
             $this->agency_model->delete($id);
             $this->session->set_flashdata('message', 'Xoá thành công');
         }
         redirect(base_url('admin/agency'));
     }
 
-    function highlight(){
+    function highlight()
+    {
         $id = $_POST['id'];
         $news = $this->news_model->get_info($id);
         $res = array("status" => 0);
-        if($news){
+        if ($news)
+        {
             $res['status'] = 1;
             $dataSubmit = array();
-            if($news->highlight){
+            if ($news->highlight)
+            {
                 $res['class'] = 'fa-toggle-off';
                 $dataSubmit['highlight'] = 0;
             }
-            else{
+            else
+            {
                 $res['class'] = 'fa-toggle-on';
                 $dataSubmit['highlight'] = 1;
             }
