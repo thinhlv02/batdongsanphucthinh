e82d587a15429dd8812053b4bb326eecf7e3b111
thinhlv <thinhluuvan@admicro.vn>
2020-01-14 15:52:49 +0700

    abc

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -37,6 +37,7 @@ Class MY_Controller extends CI_Controller
         $this->_province = $this->_func_province();
 
         $this->_template_f = TEMPLATE_FOLDER . $this->config->item('template_folder_root');
+        var_dump($this->_template_f);
 
         switch ($new_url)
         {
