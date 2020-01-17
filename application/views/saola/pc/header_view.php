b4e06fccd81389a87e0d17f1521d996a524cccac
thinhlv <thinhluuvan@admicro.vn>
2020-01-17 14:40:48 +0700

    abc

--- application/views/saola/pc/header_view.php
+++ application/views/saola/pc/header_view.php
@@ -21,7 +21,6 @@
     <meta property="og:type" content="website"/>
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">-->
     <!--    <link href="https://fonts.googleapis.com/css?family=Montserrat:200,400,700" rel="stylesheet">-->
     <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'style.css'; ?>">
     <link href="<?php echo base_url() . 'public/css/' . $template_f . 'responsive.css'; ?>" rel="stylesheet">
