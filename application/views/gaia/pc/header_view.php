7611bdf9b64b9560786b1e6135ddde97b2b6a18e
thinhlv <thinhluuvan@admicro.vn>
2020-02-07 16:44:05 +0700

    abc

--- application/views/gaia/pc/header_view.php
+++ application/views/gaia/pc/header_view.php
@@ -34,7 +34,7 @@
     <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'bootstrap.min.bs4.css'; ?>">
     <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'custom.css'; ?>">
     <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'lightslider.css'; ?>">
-    <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'jquery.bxslider.css'; ?>">
+    <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'bxslider/css/jquery.bxslider.css'; ?>">
     <link rel="stylesheet" href="<?php echo base_url() . 'public/css/' . $template_f . 'hp.css'; ?>">
     <!--End thinhlv add css-->
 
